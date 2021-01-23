b7fe02e3ab71f83ff0ae9414d68d60ec5e034d8a
lpscrypt <lpscrypt@gmail.com>
2021-01-23 13:06:45 -0500

    react table test working

--- src/components/subdomains.tsx
+++ src/components/subdomains.tsx
@@ -256,6 +256,74 @@ const Subdomains: FC<SubdomainsProps> = ({ domain: _domain }) => {
           })}
         /> */}
       </div>
+      {/* // apply the table props */}
+      <table {...getTableProps()}>
+        <thead>
+          {
+            // Loop over the header rows
+
+            headerGroups.map((headerGroup) => (
+              // Apply the header row props
+
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  // Loop over the headers in each row
+
+                  headerGroup.headers.map((column) => (
+                    // Apply the header cell props
+
+                    <th {...column.getHeaderProps()}>
+                      {
+                        // Render the header
+
+                        column.render('Header')
+                      }
+                    </th>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </thead>
+
+        {/* Apply the table body props */}
+
+        <tbody {...getTableBodyProps()}>
+          {
+            // Loop over the table rows
+
+            rows.map((row) => {
+              // Prepare the row for display
+
+              prepareRow(row);
+
+              return (
+                // Apply the row props
+
+                <tr {...row.getRowProps()}>
+                  {
+                    // Loop over the rows cells
+
+                    row.cells.map((cell) => {
+                      // Apply the cell props
+
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Render the cell contents
+
+                            cell.render('Cell')
+                          }
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
       <div
         style={{
           background: '#641f29',
