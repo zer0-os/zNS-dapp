064587c936b4ec0b650c9f44a351be7295eaa4b3
lpscrypt <lpscrypt@gmail.com>
2021-01-25 07:37:01 -0500

    cleanup

--- src/components/subdomains.tsx
+++ src/components/subdomains.tsx
@@ -139,11 +139,6 @@ const Subdomains: FC<SubdomainsProps> = ({ domain: _domain }) => {
     setSubdomainVisible(false);
   };
 
-  // const handleRowClick = (record: any) => {
-  //   history.push({
-  //     pathname: record.name,
-  //   });
-  // };
   const colors: string[] = [
     '641f29',
     'cf7571',
@@ -218,76 +213,81 @@ const Subdomains: FC<SubdomainsProps> = ({ domain: _domain }) => {
         </>
       ) : null}
       {/* // apply the table props */}
-      <table {...getTableProps()} className="subdomainsTable">
-        {rows.length === 0 ? null : (
-          <thead className="subdomainsHeaderGroup">
-            {
-              // Loop over the header rows
-              headerGroups.map((headerGroup) => (
-                // Apply the header row props
-                <tr
-                  className="subdomainsHeaderTR"
-                  {...headerGroup.getHeaderGroupProps()}
-                >
-                  {
-                    // Loop over the headers in each row
-                    headerGroup.headers.map((column) => (
-                      // Apply the header cell props
-                      <th
-                        className="subdomainsHeaderTH"
-                        {...column.getHeaderProps()}
-                      >
-                        {
-                          // Render the header
-                          column.render('Header')
-                        }
-                      </th>
-                    ))
-                  }
-                </tr>
-              ))
-            }
-          </thead>
-        )}
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {console.log('ROWS', rows)}
-
-          {
-            // Loop over the table rows
-            rows.map((row) => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr onClick={() => handleRowClick(row)} {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
+      <div className="tableContainer">
+        <table {...getTableProps()} className="subdomainsTable">
+          {rows.length === 0 ? null : (
+            <thead className="subdomainsHeaderGroup">
+              {
+                // Loop over the header rows
+                headerGroups.map((headerGroup) => (
+                  // Apply the header row props
+                  <tr
+                    className="subdomainsHeaderTR"
+                    {...headerGroup.getHeaderGroupProps()}
+                  >
+                    {
+                      // Loop over the headers in each row
+                      headerGroup.headers.map((column) => (
+                        // Apply the header cell props
+                        <th
+                          className="subdomainsHeaderTH"
+                          {...column.getHeaderProps()}
+                        >
                           {
-                            // Render the cell contents
-                            cell.render('Cell')
+                            // Render the header
+                            column.render('Header')
                           }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-        {rows.length !== 0 ? null : (
-          <tfoot>
-            <tr>
-              <td>Footer text displays here when there are no subdomains</td>
-            </tr>
-          </tfoot>
-        )}
-      </table>
+                        </th>
+                      ))
+                    }
+                  </tr>
+                ))
+              }
+            </thead>
+          )}
+          {/* Apply the table body props */}
+          <tbody {...getTableBodyProps()}>
+            {console.log('ROWS', rows)}
+
+            {
+              // Loop over the table rows
+              rows.map((row) => {
+                // Prepare the row for display
+                prepareRow(row);
+                return (
+                  // Apply the row props
+                  <tr
+                    onClick={() => handleRowClick(row)}
+                    {...row.getRowProps()}
+                  >
+                    {
+                      // Loop over the rows cells
+                      row.cells.map((cell) => {
+                        // Apply the cell props
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {
+                              // Render the cell contents
+                              cell.render('Cell')
+                            }
+                          </td>
+                        );
+                      })
+                    }
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+          {rows.length !== 0 ? null : (
+            <tfoot>
+              <tr>
+                <td>Footer text displays here when there are no subdomains</td>
+              </tr>
+            </tfoot>
+          )}
+        </table>
+      </div>
       <br />
       <br />
       {colors.map((color) => {
