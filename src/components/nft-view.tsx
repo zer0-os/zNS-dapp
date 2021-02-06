d84af71ce3fef302356a0c62beab1130d0dc7158
Joshua-Jack <Joshuajack256@gmail.com>
2021-02-06 12:46:20 -0800

    Undoing Outgoing

--- src/components/nft-view.tsx
+++ src/components/nft-view.tsx
@@ -8,7 +8,6 @@ import { Modal, Button } from 'antd';
 import Create from './create';
 import Transfer from './transferDomains';
 import Approve from './approval';
-import Outgoing from './outGoingApproval';
 
 interface ProfileProps {
   domain: string;
@@ -98,7 +97,7 @@ const NFTview: FC<ProfileProps> = ({ domain: _domain }) => {
           onCancel={profileCancel}
         >
           {domain.value.controller}
-          <Outgoing domain={_domain} />
+
           <Approve
             domain={_domain}
             outgoingPendingCount={outgoingPendingCount}
