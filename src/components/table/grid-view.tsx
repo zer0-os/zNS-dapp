666422712ec97a5a73c0be8bfe0d55c1f009a89d
Joshua-Jack <Joshuajack256@gmail.com>
2021-02-11 13:19:12 -0800

    gridView

--- src/components/table/grid-view.tsx
+++ src/components/table/grid-view.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { FC, useMemo, useState } from 'react';
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from '@web3-react/core';
+import { useZnsContracts } from '../../lib/contracts';
+import { useDomainCache } from '../../lib/useDomainCache';
 
-export const GRID = () => {
-  return <div></div>;
+interface DiscoverProps {
+  domain: string;
+}
+const Discover: FC<DiscoverProps> = ({ domain: _domain }) => {
+  const context = useWeb3React<Web3Provider>();
+  const contracts = useZnsContracts();
+  const { account } = context;
+  const { useDomain } = useDomainCache();
+  const domainContext = useDomain(_domain);
+  const { domain } = domainContext;
+
+  const GridData = useMemo(()=> if (domain.isNothing()),[])
+  return <></>;
 };
+
+export default Discover;
