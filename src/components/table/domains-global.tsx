990e5f17c8a3406980f75aa5394fa30d57f8f186
Ben Walker <ben.walker18@gmail.com>
2021-03-11 16:38:24 -0800

    Remove additional unused code

--- src/components/table/domains-global.tsx
+++ src/components/table/domains-global.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState } from 'react';
 import _ from 'lodash';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
@@ -62,9 +62,9 @@ const DomainsGlobal: FC<DomainsGlobalProps> = ({ domain: _domain }) => {
     },
   );
 
-  useEffect(() => {
-    //console.log('ChildView', domain);
-  }, [domain]);
+  //useEffect(() => {
+  //console.log('ChildView', domain);
+  //}, [domain]);
   // const showSubdomain = () => {
   //   setSubdomainVisible(true);
   // };
