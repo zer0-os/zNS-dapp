0357403b1d87380bf6f3e9d81eae863f3c6081f7
Nancy Stone <lpscrypt@gmail.com>
2021-03-21 15:01:14 -0400

    profile

--- src/components/topbar/profile/profile.tsx
+++ src/components/topbar/profile/profile.tsx
@@ -53,7 +53,7 @@ const Profile: FC = () => {
         footer={null}
         width={'50vw'}
         bodyStyle={{
-          height: '70vh',
+          height: '50vh',
           padding: '0',
           margin: '0',
         }}
@@ -72,29 +72,32 @@ const Profile: FC = () => {
                 <div className="profile-img">
                   <img src={elon} alt="" />
                 </div>
-                {/* <div className="profile-text">
-                  <div className="profile-name">Name</div>
-                  <div className="profile-url">0::/etc</div>
-                </div> */}
+                <div className="profile-text">
+                  <div className="profile-name">Elon Muskrat</div>
+                  <div className="profile-url">0::/ /wilderworld/muskrat</div>
+                </div>
               </div>
               <div className="des-container">
                 <div className="profile-des">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                   Aliquid ut tempore dolor maiores enim mollitia alias numquam
                   impedit quas ipsa odit laborum aut temporibus veritatis itaque
-                  omnis sunt, quos vitae?{' '}
+                  omnis sunt, quos vitae? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Illo molestias exercitationem provident
+                  officiis earum vitae sequi laudantium sed, eius culpa ex
+                  voluptatem explicabo sapiente, itaque voluptate neque repellat
+                  asperiores. Fugiat.
                 </div>
               </div>
             </div>
 
-            <div className="footer-container">
+            {/* <div className="footer-container">
               <div className="footer-qr">
                 <div className="qr">
                   <img src={qr} alt="" />
                 </div>
               </div>
               <div className="footer-right">
-                {/* <div className="ethLogo">Eth</div> */}
                 <div className="footer-text">Your Ethereum Address</div>
                 <div className="footer-address">
                   <div className="eth-btn">
@@ -108,6 +111,11 @@ const Profile: FC = () => {
                   </div>
                 </div>
               </div>
+            </div> */}
+          </div>
+          <div className="savePadding">
+            <div className="saveButton">
+              <div>SAVE</div>
             </div>
           </div>
         </div>
