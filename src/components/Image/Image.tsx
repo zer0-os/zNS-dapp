3b2bed094695c976c526c5cdf907dcdd517dca7a
Brett <colbr243@gmail.com>
2021-05-01 00:24:41 +1200

    Image fade-in

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -8,11 +8,18 @@ const Image = (props: any) => {
     const load = () => setLoaded(true)
     const unload = () => setLoaded(false)
 
+    // return (
+    //     <>
+    //         <img {...props} className={props.className + ' ' + styles.LazyImage} style={{...props.style}} onLoad={load} src={props.src} />
+    //         { !loaded && <div {...props} className={props.className + ' ' + styles.Loading}><div className={`${styles.Spinner} ${loaded ? styles.Expand : ''}`}></div></div> }
+    //     </>
+    // )
+
     return (
-        <>
-            <img {...props} className={props.className + ' ' + styles.LazyImage} style={{...props.style, display: loaded ? 'inherit' : 'none'}} onLoad={load} src={props.src} />
-            { !loaded && <div {...props} className={props.className + ' ' + styles.Loading}><div className={styles.Spinner}></div></div> }
-        </>
+        <div style={{...props.style}} className={styles.LazyImage}>
+            <img {...props} onLoad={load} style={{...props.style, opacity: loaded ? 1 : 0}} className={`${props.className}`} />
+            <div {...props} className={props.className + ' ' + styles.Indicator}><div className={styles.Spinner}></div></div>
+        </div>
     )
 }
 
