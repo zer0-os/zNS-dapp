eb21dc7fb80ce96fa569da48c034bdfc761a66f3
Brett <colbr243@gmail.com>
2021-05-07 17:21:00 +1200

    FilterBar now disappears on scroll down

--- src/components/NavBars/FilterBar/FilterBar.tsx
+++ src/components/NavBars/FilterBar/FilterBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import useScrollPosition from '@react-hook/window-scroll';
 
 import styles from './FilterBar.module.css'
 
@@ -11,9 +12,23 @@ type FilterBarProps = {
     children?: React.ReactNode;
 }
 
+var lastY = 0 // Just a global variable to stash last scroll position
+
 const FilterBar: React.FC<FilterBarProps> = ({ filters, onSelect, style, children }) => {
 
     const [ selected, setSelected ] = useState(filters.length ? filters[0] : '')
+
+    // TODO: Move hidden header to a separate component
+    const [hideHeader, setHideHeader] = useState(false)
+    const handleScroll = () => {
+        const hide = window.pageYOffset > 100 && window.pageYOffset > lastY
+        lastY = window.pageYOffset
+        setHideHeader(hide)
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [ hideHeader ])
 
     const select = (filter: string) => {
         setSelected(filter)
@@ -21,7 +36,7 @@ const FilterBar: React.FC<FilterBarProps> = ({ filters, onSelect, style, childre
     }
 
     return(
-        <div className={`${styles.FilterBar} blur`} style={style}>
+        <div className={`${styles.FilterBar} blur ${hideHeader ? styles.Hidden : ''}`} style={style}>
             { children }
             <ul>
                 { filters.map((filter, index) => 
