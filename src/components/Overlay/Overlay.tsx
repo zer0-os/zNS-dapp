5efc783a1986788b1e7e3dc9c7c6c7e0bbe888e6
Brett <colbr243@gmail.com>
2021-05-07 17:21:11 +1200

    Small edits on overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -7,12 +7,15 @@ type OverlayProps = {
 
 const Overlay: React.FC<OverlayProps> = ({ onClose, children }) => {
 
+	// TODO: Some overlays should be centered, other's should be top-aligned (with some padding of course)
+
 	const closeOverlay = (e: React.MouseEvent) => {
 		const target = e.target as HTMLInputElement
 		if(target.classList.value.indexOf('overlay') > -1) onClose()
 	}
 
 	return (
+		<>
 		<div onClick={closeOverlay} style={{
 			width: '100%', minHeight: '100vh', 
 			display: 'flex', alignItems: 'center', 
@@ -26,6 +29,8 @@ const Overlay: React.FC<OverlayProps> = ({ onClose, children }) => {
 		}} className='blur overlay'>
 			{children}
 		</div>
+		<h1>Overlay scrolling is a little broken right now - this will be fixed</h1>
+		</>
 	)
 }
 
