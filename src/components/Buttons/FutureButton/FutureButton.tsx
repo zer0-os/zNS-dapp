731de5ab1f77d24d14ef25d58de8b140641d0fb3
Brett <colbr243@gmail.com>
2021-05-29 17:53:33 +1200

    Added a loading state for FutureButton

--- src/components/Buttons/FutureButton/FutureButton.tsx
+++ src/components/Buttons/FutureButton/FutureButton.tsx
@@ -8,23 +8,27 @@ type FutureButtonProps = {
 	toggleable?: boolean;
 	children: React.ReactNode;
 	glow?: boolean;
+	loading?: boolean;
 };
 
+// @TODO Should make glow the default state since it's much more prevalent in the design
 const FutureButton: React.FC<FutureButtonProps> = ({
 	onClick,
 	style,
 	toggleable,
 	children,
 	glow,
+	loading,
 }) => {
 	const [hasHovered, setHovered] = useState(false);
 	const [isSelected, setSelected] = useState(false);
 
 	const handleHover = () => {
-		setHovered(true);
+		if (!hasHovered) setHovered(true);
 	};
 
 	const handleClick = () => {
+		if (loading) return;
 		if (onClick) onClick();
 		if (toggleable) setSelected(!isSelected);
 	};
@@ -33,12 +37,15 @@ const FutureButton: React.FC<FutureButtonProps> = ({
 		<button
 			className={`${styles.futureButton} ${isSelected ? styles.selected : ''} ${
 				glow ? styles.glow : ''
-			}`}
+			} ${loading ? styles.Loading : ''}`}
 			onMouseEnter={handleHover}
 			onMouseUp={handleClick}
 			style={style}
 		>
-			<div className={styles.content}>{children}</div>
+			<div className={styles.content}>
+				{!loading && children}
+				{loading && <div className={styles.Spinner}></div>}
+			</div>
 			<div
 				className={`${styles.wash} ${
 					hasHovered && !isSelected ? styles.hovered : ''
