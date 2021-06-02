c63cc1300accfcc9746d0e0e3ea60613649fecce
Brett <colbr243@gmail.com>
2021-06-02 17:05:19 +1200

    Pushed all enlisting to a provider

---
+++ src/lib/providers/EnlistProvider.tsx
@@ -0,0 +1,55 @@
+//- React Imports
+import React, { useState, useEffect } from 'react';
+
+//- Type Imports
+import { Domain, DefaultDomain } from 'lib/types';
+
+type context = {
+	enlisting: Domain | undefined;
+	enlist: (domain: Domain) => void;
+	submit: () => void;
+	clear: () => void;
+};
+
+export const EnlistContext = React.createContext<context>({
+	enlisting: DefaultDomain || undefined,
+	enlist: (domain: Domain) => {},
+	submit: () => {},
+	clear: () => {},
+});
+
+type EnlistProviderType = {
+	children: React.ReactNode;
+};
+
+const EnlistProvider: React.FC<EnlistProviderType> = ({ children }) => {
+	const [enlisting, setEnlisting] = useState<Domain | undefined>(undefined);
+
+	const enlist = (domain: Domain) => {
+		setEnlisting(domain);
+	};
+
+	const clear = () => setEnlisting(undefined);
+
+	// @TODO Set up enlisting back-end
+	const submit = () => {
+		console.log('hello');
+		console.warn('Enlisting is not yet linked up');
+		clear();
+	};
+
+	const contextValue = {
+		enlisting,
+		enlist,
+		submit,
+		clear,
+	};
+
+	return (
+		<EnlistContext.Provider value={contextValue}>
+			{children}
+		</EnlistContext.Provider>
+	);
+};
+
+export default EnlistProvider;
