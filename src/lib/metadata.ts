f2df049347a11887926a1d61503d63667679da54
Remscar <remscar@gmail.com>
2021-06-14 11:49:20 -0700

    Made getMetadata return a type and use a basic memory cache

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,12 +1,30 @@
-export async function getMetadata(metadataUrl: string) {
+import { Metadata } from './types';
+
+interface MetadataCache {
+	[url: string]: Metadata | undefined;
+}
+
+const memoryCache: MetadataCache = {};
+
+export async function getMetadata(
+	metadataUrl: string,
+): Promise<Metadata | undefined> {
 	try {
+		if (memoryCache[metadataUrl]) {
+			return memoryCache[metadataUrl];
+		}
+
 		const response = await fetch(metadataUrl);
 		const data = await response.json();
-		return {
+		const metadata = {
 			title: data.name || data.title,
 			description: data.description,
 			image: data.image,
-		};
+		} as Metadata;
+
+		memoryCache[metadataUrl] = metadata;
+
+		return metadata;
 	} catch (e) {
 		console.error('Failed to retrieve metadata from ' + metadataUrl);
 		return;
