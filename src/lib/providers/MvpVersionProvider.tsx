037a0acedcf1909b172427bd3ecdc9654a61ab20
Brett <colbr243@gmail.com>
2021-06-16 00:35:45 +1200

    More accurate naming for global MVP version function

--- src/lib/providers/MvpVersionProvider.tsx
+++ src/lib/providers/MvpVersionProvider.tsx
@@ -11,12 +11,28 @@ type MvpVersionProviderType = {
 
 // Console message to let users know they can switch MVP versions
 console.log(
-	'%cHello fellow devs and tinkerers! If you would like to try different MVP versions, you can call mvp( [version] ) in the console, i.e. mvp(3)\nValid versions: 1, 2, 3',
+	'%cHello fellow devs and tinkerers! If you would like to try different versions of this dapp, production() or prototype()\nCurrent Mode: Production',
 	'display: block; border: 3px solid #3ca1ff; border-radius: 7px; padding: 10px; margin: 8px;',
 );
 
 const MvpVersionProvider: React.FC<MvpVersionProviderType> = ({ children }) => {
 	const [mvpVersion, set] = useState(1);
+
+	const production = () => {
+		console.log(
+			'%cYou have switched to production mode',
+			'display: block; border: 3px solid #3ca1ff; border-radius: 7px; padding: 10px; margin: 8px;',
+		);
+		set(1);
+	};
+
+	const prototype = () => {
+		console.log(
+			'%cYou have switched to prototype mode',
+			'display: block; border: 3px solid #3ca1ff; border-radius: 7px; padding: 10px; margin: 8px;',
+		);
+		set(3);
+	};
 
 	const setMvpVersion = (version: number) => {
 		if (version > 3 || version < 1) {
@@ -36,6 +52,8 @@ const MvpVersionProvider: React.FC<MvpVersionProviderType> = ({ children }) => {
 
 	// Global function so it can be called in the console
 	(global as any).mvp = setMvpVersion;
+	(global as any).production = production;
+	(global as any).prototype = prototype;
 
 	return (
 		<MvpVersionContext.Provider value={contextValue}>
