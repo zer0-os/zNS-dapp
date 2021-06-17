680e90c3c78468c2e67ce37baaf5c7fee09d6485
Brett <colbr243@gmail.com>
2021-06-17 13:33:17 +1200

    Fixed linter warning

--- src/lib/providers/StakingRequestProvider.tsx
+++ src/lib/providers/StakingRequestProvider.tsx
@@ -1,5 +1,5 @@
 //- React Imports
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 //- Hook Imports
 import useNotification from 'lib/hooks/useNotification';
