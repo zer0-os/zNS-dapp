c6b464280aa8dbcd4b351fb0caa9de8806088fb3
Brett <colbr243@gmail.com>
2021-07-08 00:58:39 +1200

    Added wildToUsd function

--- src/lib/coingecko.ts
+++ src/lib/coingecko.ts
@@ -10,3 +10,18 @@ export const tokenToUsd = (token: string) => {
 			});
 	});
 };
+
+// @todo caching
+// @todo regularly poll for USD price
+export const wildToUsd = async (amount: number) => {
+	try {
+		const response = await fetch(
+			`https://api.coingecko.com/api/v3/simple/price?ids=wilder-world&vs_currencies=usd`,
+		);
+		const conversion = (await response.json())['wilder-world'].usd;
+		return amount * conversion;
+	} catch (e) {
+		console.error('Failed to retrieve price data for WILD token');
+		return;
+	}
+};
