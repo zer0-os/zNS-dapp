f34fb111412ee2e20570ad506bffa9ffbe5d611e
Leonel Salvo <agustin.leonel.salvo@hotmail.com>
2021-07-15 17:28:35 -0300

    Created useApprovals hook with both approval methods for registrar

---
+++ src/lib/hooks/useAprovals.tsx
@@ -0,0 +1,65 @@
+import { useZnsContracts } from 'lib/contracts';
+import { ethers } from 'ethers';
+import { tryFunction} from 'lib/utils';
+
+ export interface SingleApprovalParams {
+	operator: string;
+	tokenId: number;
+}
+
+ export interface ForAllApprovalParams {
+	operator: string;
+	bool: boolean;
+}
+
+interface ApproveNFTHook {
+	sendSingleApproval: (
+		params: SingleApprovalParams,
+	) => Promise<ethers.ContractTransaction>;
+	sendApprovalForAll: (
+		params: ForAllApprovalParams,
+	) => Promise<ethers.ContractTransaction>;
+}
+
+export function useApprovals(): ApproveNFTHook {
+    
+	const registry = useZnsContracts()?.registry;
+
+	const sendSingleApproval = async (params: SingleApprovalParams) => {
+		const tx = await tryFunction(async () => {
+			if (!registry) {
+				throw Error(`no registry`);
+			}
+
+			// Send the Approval for a single NFT
+			const tx = await registry.approve(
+				params.operator,
+				params.tokenId,
+			);
+
+			return tx;
+		}, `Send Single Approval`);
+
+		return tx;
+	};
+
+	const sendApprovalForAll = async (params: ForAllApprovalParams) => {
+		const tx = await tryFunction(async () => {
+			if (!registry) {
+				throw Error(`no registry`);
+			}
+
+			// Send the Approval for All
+			const tx = await registry.setApprovalForAll(
+				params.operator,
+				params.bool,
+			);
+
+			return tx;
+		}, `Send Approval for All`);
+
+		return tx;
+	};
+
+	return { sendSingleApproval , sendApprovalForAll };
+}
