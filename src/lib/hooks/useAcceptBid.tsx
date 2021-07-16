0235fdd033c5feb9175a7012ed7216514cdb13f5
Leonel Salvo <agustin.leonel.salvo@hotmail.com>
2021-07-15 21:04:09 -0300

    Added useAcceptBid

---
+++ src/lib/hooks/useAcceptBid.tsx
@@ -0,0 +1,49 @@
+import { useZnsContracts } from 'lib/contracts';
+import { tryFunction } from 'lib/utils';
+import { ethers } from 'ethers';
+
+export interface AcceptBidParams {
+	signature: ethers.BytesLike;
+	auctionId: number;
+	bidder: string;
+	bid: number;
+	nftAddress: string;
+	tokenId: number;
+	minBid: number;
+	startBlock: number;
+	expireBlock: number;
+}
+
+interface StakingControllerHooks {
+	acceptBid: (params: AcceptBidParams) => Promise<ethers.ContractTransaction>;
+}
+
+export function useAcceptBid(): StakingControllerHooks {
+	const zAuctionContract = useZnsContracts()?.zAuction;
+
+	const acceptBid = async (params: AcceptBidParams) => {
+		const tx = await tryFunction(async () => {
+			if (!zAuctionContract) {
+				throw Error(`no contract`);
+			}
+
+			const tx = await zAuctionContract.acceptBid(
+				params.signature,
+				params.auctionId,
+				params.bidder,
+				params.bid,
+				params.nftAddress,
+				params.tokenId,
+                params.minBid,
+				params.startBlock,
+				params.expireBlock,
+			);
+
+			return tx;
+		}, 'approve request');
+
+		return tx;
+	};
+
+	return { acceptBid };
+}
