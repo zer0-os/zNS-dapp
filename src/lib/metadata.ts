aea66da27fa801ed12f58346032a39346c3db06d
Brett <colbr243@gmail.com>
2021-08-02 15:34:16 +1200

    Re-enabled real data

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -14,32 +14,21 @@ export async function getMetadata(
 			return memoryCache[metadataUrl];
 		}
 
-		await new Promise((r) => setTimeout(r, 1500));
-		return {
-			title: 'mock title',
-			description: 'mock description',
-			image: `https://picsum.photos/seed/${metadataUrl.substring(
-				21,
-			)}/${Math.floor(Math.random() * 1000)}/${Math.floor(
-				Math.random() * 1000,
-			)}`,
-		};
+		const response = await fetch(metadataUrl);
+		const data = await response.json();
+		const metadata = {
+			title: data.name || data.title,
+			description: data.description,
+			image: data.image,
+		} as Metadata;
 
-		// const response = await fetch(metadataUrl);
-		// const data = await response.json();
-		// const metadata = {
-		// 	title: data.name || data.title,
-		// 	description: data.description,
-		// 	image: data.image,
-		// } as Metadata;
+		if (!metadata.title || !metadata.description || !metadata.image) {
+			throw Error();
+		}
 
-		// if (!metadata.title || !metadata.description || !metadata.image) {
-		// 	throw Error();
-		// }
+		memoryCache[metadataUrl] = metadata;
 
-		// memoryCache[metadataUrl] = metadata;
-
-		// return metadata;
+		return metadata;
 	} catch (e) {
 		console.error('Failed to retrieve metadata from ' + metadataUrl);
 		return;
