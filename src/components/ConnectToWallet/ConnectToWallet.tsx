39f5808643f8f76d2867883c998464367f8a72d8
Leonel Salvo <agustin.leonel.salvo@hotmail.com>
2021-08-11 23:35:24 -0300

    Reload after failing to connect

--- src/components/ConnectToWallet/ConnectToWallet.tsx
+++ src/components/ConnectToWallet/ConnectToWallet.tsx
@@ -81,6 +81,7 @@ const ConnectToWallet: React.FC<ConnectToWalletProps> = ({ onConnect }) => {
 
 			await activate(c, async (e: Error) => {
 				addNotification(`Failed to connect to wallet.`);
+				window.location.reload()
 				localStorage.removeItem('chosenWallet')
 				console.error(`Encounter error while connecting to ${wallet}.`);
 				console.error(e);
