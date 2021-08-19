eb9c91a6395bd766bed2c264efe89793b7d64dd3
Brett Collins <colbr243@gmail.com>
2021-08-19 13:25:32 +1200

    Added view/accept bids functionality back

---
+++ src/components/Tables/DomainTable/components/ViewBids.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from 'react';
+
+import { useBidProvider } from 'lib/providers/BidProvider';
+
+import { Spinner } from 'components';
+
+import { Bid, Domain, DomainData } from 'lib/types';
+import FutureButton from 'components/Buttons/FutureButton/FutureButton';
+
+type ViewBidsProps = {
+	domain: Domain;
+	onClick: (domain: DomainData) => void;
+	style?: React.CSSProperties;
+};
+
+const ViewBids: React.FC<ViewBidsProps> = ({ domain, onClick, style }) => {
+	let isMounted = useRef(false);
+	const { getBidsForDomain } = useBidProvider();
+	const [bids, setBids] = useState<Bid[] | undefined>();
+
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	const handleClick = () => {
+		if (!isLoading && bids !== undefined) {
+			onClick({
+				domain,
+				bids,
+			});
+		}
+	};
+
+	const getBids = async () => {
+		const bids = await getBidsForDomain(domain);
+		if (!isMounted.current) return;
+		setIsLoading(false);
+		if (bids && bids.length) setBids(bids);
+	};
+
+	useEffect(() => {
+		isMounted.current = true;
+		setIsLoading(true);
+		getBids();
+		return () => {
+			isMounted.current = false;
+		};
+	}, [domain]);
+
+	return (
+		<FutureButton
+			onClick={handleClick}
+			glow={!isLoading && bids !== undefined}
+			style={style}
+		>
+			View Bids
+		</FutureButton>
+	);
+};
+
+export default ViewBids;
