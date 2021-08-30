4b4dca1068e68b075371fd7a95ecec13b60a9c8a
Leonel Salvo <agustin.leonel.salvo@hotmail.com>
2021-08-30 20:00:15 -0300

    Added capital letter

--- src/components/Tables/DomainTable/components/ViewBids.tsx
+++ src/components/Tables/DomainTable/components/ViewBids.tsx
@@ -61,7 +61,7 @@ const ViewBids: React.FC<ViewBidsProps> = ({
 				</FutureButton>
 			)}
 			{bids === undefined && (
-				<div style={{ textAlign: 'right', marginRight: '48px' }}>no bids</div>
+				<div style={{ textAlign: 'right', marginRight: '48px' }}>No bids</div>
 			)}
 		</>
 	);
