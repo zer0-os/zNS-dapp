bdd264c20ecbb9406a8d83e4d381c49c28024c4e
colbr <colbr243@gmail.com>
2021-09-08 14:40:53 +1200

    Rolled back to old version of Image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,120 +1,29 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 
 import styles from './Image.module.css';
+// import placeholder from './'
 
-enum MediaType {
-	Image,
-	Video,
-}
-
-// Note:
-// This component is being re-written in another
-// branch - this is just a hotfix to get videos
-// working on mobile
+// @TODO: Refactor props to not by 'any' type
 const Image = (props: any) => {
-	const isMounted = useRef(false);
-	const refVideo = useRef<HTMLVideoElement | undefined>(null);
 	const [loaded, setLoaded] = useState(false);
-
-	const [mediaType, setMediaType] = useState<MediaType | undefined>();
-	const [loadedSrc, setLoadedSrc] = useState<string | undefined>();
-	const [loadingSrc, setLoadingSrc] = useState<string | undefined>();
+	const [tryVideo, setTryVideo] = useState(false);
 
 	const containerRef = useRef<HTMLDivElement>(null);
 
-	///////////////
-	// Functions //
-	///////////////
+	const click = (e: any) => {
+		if (!loaded || (tryVideo && props.controls)) {
+			e.stopPropagation();
+		} else {
+			if (props.onClick) props.onClick();
+		}
+	};
 
 	const load = () => setLoaded(true);
-
-	const getMediaType = (typeFromBlob: string) => {
-		if (typeFromBlob.indexOf('image') > -1) {
-			return MediaType.Image;
-		} else if (typeFromBlob.indexOf('video') > -1) {
-			return MediaType.Video;
-		} else {
-			return undefined;
+	const err = (e: any) => {
+		if (props.src) {
+			setTryVideo(true);
 		}
 	};
-
-	const click = (event: any) => {
-		if (!loaded || (mediaType === MediaType.Video && props.controls)) {
-			event.stopPropagation();
-		}
-	};
-
-	/////////////
-	// Effects //
-	/////////////
-
-	useEffect(() => {
-		isMounted.current = true;
-		return () => {
-			isMounted.current = false;
-		};
-	}, []);
-
-	/* Get type of media in src
-		 There's no indication from the IPFS url as to whether
-		 we're loading an image or a video, which is important
-		 for rendering the correct tags.
-
-		 Fetch the blob first and check file type before rendering
-	*/
-	useEffect(() => {
-		if (
-			!props.src ||
-			props.src.length === 0 ||
-			props.src === loadedSrc ||
-			(loadingSrc && props.src === loadingSrc)
-		) {
-			return;
-		}
-
-		// Hotfix for SVGs
-		if (props.src.indexOf('.svg') > -1) {
-			setLoaded(true);
-			setLoadingSrc(props.src);
-			setMediaType(MediaType.Image);
-			setLoadedSrc(props.src);
-			return;
-		}
-
-		setLoaded(false);
-		setLoadingSrc(props.src);
-		fetch(props.src).then(async (r: Response) => {
-			// Check that the URL hasn't switched between
-			// making the fetch and receiving data
-			if (r.url !== props.src || !isMounted.current) {
-				return;
-			}
-			if (r.status !== 200) {
-				throw 'Failed to retrieve media data at ' + props.src;
-			}
-
-			try {
-				const blob = await r.blob();
-
-				// Check again since we had an async call
-				if (r.url !== props.src || !isMounted.current) {
-					return;
-				}
-
-				const type = getMediaType(blob.type);
-				setMediaType(type);
-				setLoadedSrc(r.url);
-				setLoadingSrc(undefined);
-			} catch (e: any) {
-				console.error(e);
-				return;
-			}
-		});
-	}, [props.src]);
-
-	////////////
-	// Render //
-	////////////
 
 	return (
 		<div
@@ -130,11 +39,11 @@ const Image = (props: any) => {
 			onClick={click}
 		>
 			{!loaded && (
-				<div {...props} className={styles.Loading}>
+				<div {...props} onClick={click} className={styles.Loading}>
 					<div className={styles.Spinner}></div>
 				</div>
 			)}
-			{mediaType === MediaType.Image && (
+			{!tryVideo && (
 				<img
 					{...props}
 					className={`${props.className ? props.className : ''} ${
@@ -142,20 +51,21 @@ const Image = (props: any) => {
 					}`}
 					style={{
 						opacity: loaded ? 1 : 0,
+						objectFit: 'cover',
 						...props.style,
 					}}
 					onLoad={load}
 					src={props.src}
 					alt={props.alt || ''}
+					onError={err}
 					onClick={click}
 				/>
 			)}
-			{mediaType === MediaType.Video && (
+			{tryVideo && (
 				<video
 					{...props}
-					autoPlay="auto"
-					loop
-					ref={refVideo}
+					autoPlay
+					muted
 					className={`${props.className ? props.className : ''} ${
 						styles.Image
 					} ${styles.Video}`}
@@ -164,11 +74,8 @@ const Image = (props: any) => {
 						objectFit: 'cover',
 						...props.style,
 					}}
-					src={props.src}
 					preload="metadata"
 					onLoadedMetadata={load}
-					muted
-					playsInline
 					onClick={click}
 				/>
 			)}
