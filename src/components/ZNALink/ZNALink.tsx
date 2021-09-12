567c1d0787358ee7641476b12386697bf91a8109
JeffersonMat <jeff_08062011@hotmail.com>
2021-09-12 22:02:45 +1200

    added alt-link to link and removed style colour

--- src/components/ZNALink/ZNALink.tsx
+++ src/components/ZNALink/ZNALink.tsx
@@ -16,7 +16,7 @@ const ZNALink: React.FC<ZNAProps> = ({ className, domain, style }) => {
 			className={`${styles.ZNALink} ${className ? className : ''}`}
 			style={style}
 		>
-			<a href='https://www.zero.tech/'  target="_blank" rel="noreferrer" style={{ cursor: 'cursor', opacity: 0.75, color: 'white' }}>0://</a>
+			<a className="alt-link" href='https://www.zero.tech/'  target="_blank" rel="noreferrer" style={{ cursor: 'cursor', opacity: 0.75 }}>0://</a>
 			<Link style={{ textDecoration: 'none', color: 'white' }} to={''}>
 				wilder
 			</Link>
