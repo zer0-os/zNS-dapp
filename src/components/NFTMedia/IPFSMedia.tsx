b65230cc147b081a56fe42aab9fa61256bc13982
colbr <colbr243@gmail.com>
2021-09-12 14:25:25 +1200

    Added fix for iOS where videos without Cloudinary link wouldn't play

--- src/components/NFTMedia/IPFSMedia.tsx
+++ src/components/NFTMedia/IPFSMedia.tsx
@@ -1,4 +1,5 @@
 // Style Imports
+import { useEffect, useState } from 'react';
 import styles from './NFTMedia.module.css';
 
 // Local Imports
@@ -9,11 +10,13 @@ const IPFSMedia = (props: MediaContainerProps) => {
 	// State & Data //
 	//////////////////
 
-	const { alt, ipfsUrl, isVideo, onLoad, size, style } = props;
+	const { alt, ipfsUrl, onLoad, size, style } = props;
 
 	///////////////
 	// Functions //
 	///////////////
+
+	const [imageFailed, setImageFailed] = useState<boolean>(false);
 
 	// Click handler for videos
 	// Prevents prop onClick if controls
@@ -27,23 +30,37 @@ const IPFSMedia = (props: MediaContainerProps) => {
 		}
 	};
 
+	// This is a hack to get iOS videos working
+	// iOS doesn't like loading URLs with no file extension into
+	// video tags, however it doesn't mind showing them in img tags.
+	// So first, we try to show the content in an image tag, and if that fails
+	// (which it will everywhere apart from iOS) try it in a video tag
+	const failedToLoadInImageTag = () => {
+		setImageFailed(true);
+	};
+
+	useEffect(() => {
+		setImageFailed(false);
+	}, [ipfsUrl]);
+
 	///////////////
 	// Fragments //
 	///////////////
 
 	return (
 		<>
-			{!isVideo && (
+			{!imageFailed && (
 				<img
 					alt={alt}
 					className={styles.Media}
 					onClick={props.onClick}
+					onError={failedToLoadInImageTag}
 					onLoad={onLoad}
 					src={ipfsUrl}
 					style={style}
 				/>
 			)}
-			{isVideo && (
+			{imageFailed && (
 				<video
 					autoPlay
 					className={styles.Media}
