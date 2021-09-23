775bd492953ddd40da766f86b93982ae000777dd
alejoviola <viola.alejo@outlook.com>
2021-09-22 23:43:46 -0300

    Setting Image Render v2

--- src/scenes/generateJpg/GenerateJpg.tsx
+++ src/scenes/generateJpg/GenerateJpg.tsx
@@ -20,7 +20,7 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 	// States //
 	////////////
 
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState<Boolean>(true);
 
 	///////////////////////////////////////
 
@@ -29,7 +29,7 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 
 	//Camera Viewer
 	const [camera, setCamera] = useState(
-		new Three.PerspectiveCamera(75, 240 / 240, 0.1, 1000),
+		new Three.PerspectiveCamera(75, 224 / 224, 0.1, 1000),
 	);
 
 	camera.position.set(-5, 1, 10);
@@ -39,12 +39,7 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 
 	// Renderer Viewport
 	const renderer = new Three.WebGLRenderer({ antialias: true });
-	renderer.setSize(240, 240);
-
-	useEffect(() => {
-		const faso = document.getElementById('Container3d');
-		faso?.appendChild(renderer.domElement);
-	}, []);
+	renderer.setSize(224, 224);
 
 	// tone mapping
 	renderer.toneMapping = Three.NoToneMapping;
@@ -52,9 +47,7 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 
 	///////////////////////////////////////
 
-	var img = document.createElement('img');
-
-	var picture: string;
+	const [img, setImg] = useState(document.createElement('img'));
 
 	//ENV MAP
 	new RGBELoader().setPath('assets/hdri/').load(
@@ -66,40 +59,16 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 				function (gltf) {
 					scene.add(gltf.scene);
 
-					const cameraRender = new Three.PerspectiveCamera(
-						200,
-						300 / 300,
-						0.1,
-						1000,
-					);
+					renderer.render(scene, camera);
 
-					cameraRender.position.set(
-						camera.position.x,
-						camera.position.y,
-						camera.position.z,
-					);
-
-					cameraRender.rotation.set(
-						camera.rotation.x,
-						camera.rotation.y,
-						camera.rotation.z,
-					);
-
-					renderer.setSize(300, 300);
-					renderer.render(scene, cameraRender);
-
-					var binaryData = [];
-					binaryData.push(renderer.domElement.toDataURL('image/png'));
-					picture = URL.createObjectURL(
-						new Blob(binaryData, { type: 'image/png' }),
-					);
-					console.log(picture.split('blob:').pop());
-					let source: string | undefined = picture.split('blob:').pop();
-					img.src = source ? source : '';
+					img.src = renderer.domElement.toDataURL('image/png');
 					img.alt = 'NFT Preview';
 					const image = document.getElementById('Container3d');
+					if (image?.lastChild) {
+						image?.removeChild(image?.lastChild);
+					}
+					console.log('Faso');
 					image?.appendChild(img);
-					console.log(img);
 					setLoading(false);
 				},
 				undefined,
@@ -110,7 +79,6 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 
 			texture.mapping = Three.EquirectangularReflectionMapping;
 
-			scene.background = texture;
 			scene.environment = texture;
 		},
 		undefined,
@@ -121,53 +89,15 @@ const GenerateJpg: React.FC<GenerateJpgProps> = ({ url }) => {
 
 	///////////////////////////////////////
 
-	//Function that reload
-	function animate() {
-		//Update Size and Render
-		renderer.render(scene, camera);
-
-		//Loop
-		requestAnimationFrame(animate);
-	}
-	animate();
-
-	/////////////////
-	// SAVE RENDER //
-	/////////////////
-
-	const takeScreenShot = () => {
-		//Camera Render
-		const cameraRender = new Three.PerspectiveCamera(200, 300 / 300, 0.1, 1000);
-
-		cameraRender.position.set(
-			camera.position.x,
-			camera.position.y,
-			camera.position.z,
-		);
-
-		cameraRender.rotation.set(
-			camera.rotation.x,
-			camera.rotation.y,
-			camera.rotation.z,
-		);
-
-		renderer.setSize(300, 300);
-		renderer.render(scene, cameraRender);
-
-		/*
-		a.href = renderer.domElement.toDataURL('image/png');
-		a.download = 'screenshot.png';
-		a.click();*/
-	};
-
 	return (
 		<div
 			id={'Container3d'}
 			style={{ display: 'inline-block', position: 'relative' }}
 		>
 			{loading && <div id="loader"></div>}
+			<div></div>
 
-			{/*!loading && <img alt="NFT Preview" src={picture} />*/}
+			{/*!loading && <div></div>*/}
 		</div>
 	);
 };
