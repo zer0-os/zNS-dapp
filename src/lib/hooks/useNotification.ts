e0bf8cc6d44a99b8c3b394e701207d92d8ee0836
ericlow <eric.l@turing.com>
2021-10-06 19:23:38 -0600

    Impement redux saga into the project and switched the Notification to redux from context

--- src/lib/hooks/useNotification.ts
+++ src/lib/hooks/useNotification.ts
@@ -1,10 +1,49 @@
-import { useContext } from 'react';
-import { NotificationContext } from 'lib/providers/NotificationProvider';
+import { useMemo, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	addNotificationRequest,
+	removeNotificationRequest,
+} from 'store/notifications/actions';
+import { getNotifications } from 'store/notifications/selectors';
+import { randomUUID } from 'lib/Random';
+import { DEFAULT_NOTIFICATION_DURATION } from 'constants/notifications';
 
-function useNotification() {
-	const { notifications, addNotification, removeNotification } =
-		useContext(NotificationContext);
-	return { notifications, addNotification, removeNotification };
-}
+const useNotification = () => {
+	const notifications = useSelector(getNotifications);
+
+	const dispatch = useDispatch();
+	const addNotification = useCallback(
+		(text, duration = DEFAULT_NOTIFICATION_DURATION) => {
+			dispatch(
+				addNotificationRequest({
+					id: randomUUID(),
+					text,
+					duration,
+				}),
+			);
+		},
+		[dispatch],
+	);
+
+	const removeNotification = useCallback(
+		(id) => {
+			dispatch(
+				removeNotificationRequest({
+					id,
+				}),
+			);
+		},
+		[dispatch],
+	);
+
+	return useMemo(
+		() => ({
+			notifications,
+			addNotification,
+			removeNotification,
+		}),
+		[notifications, addNotification, removeNotification],
+	);
+};
 
 export default useNotification;
