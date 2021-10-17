4f68d7527dfa2bdd46a09d47070f210f91fad7b9
Brett <colbr243@gmail.com>
2021-10-18 09:56:36 +1300

    fix: adjusted copy

--- src/containers/flows/WheelsRaffle/index.tsx
+++ src/containers/flows/WheelsRaffle/index.tsx
@@ -76,7 +76,7 @@ const WheelsRaffleContainer = () => {
 		} else if (hasRaffleStarted) {
 			return (
 				<>
-					Wilder Wheels raffle has started - whitelist sale starting in{' '}
+					Wilder Wheels whitelist raffle has started - sale starting in{' '}
 					<b>
 						<Countdown
 							to={RAFFLE_END_TIME}
