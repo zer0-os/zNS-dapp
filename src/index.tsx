1f0c7d6248b9c1aff86b3bdee55bb21d1a3500f9
alejoviola <viola.alejo@outlook.com>
2021-10-18 15:31:24 -0300

    Adding React-Snap & Changing index

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<head>
-			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-		</head>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+import { hydrate, render } from 'react-dom';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement?.hasChildNodes()) {
+	hydrate(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		rootElement,
+	);
+} else {
+	render(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		rootElement,
+	);
+}
 
 reportWebVitals();
