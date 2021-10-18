61492c75c261f5c9e8de95d960587d8156535ae3
alejoviola <viola.alejo@outlook.com>
2021-10-18 12:07:12 -0300

    Revert "Adding React-Snap"
    This reverts commit efbe00dfa77452603d9dc66d701f5333f07cd8c6.

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//Adding Hydrate for Twitter Card update
-import { hydrate } from 'react-dom';
-
-const rootElement = document.getElementById('root');
-if (rootElement?.hasChildNodes()) {
-	hydrate(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		document.getElementById('root'),
-	);
-} else {
-	ReactDOM.render(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		document.getElementById('root'),
-	);
-}
+ReactDOM.render(
+	<React.StrictMode>
+		<head>
+			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+		</head>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
 
 reportWebVitals();
