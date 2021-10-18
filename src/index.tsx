efbe00dfa77452603d9dc66d701f5333f07cd8c6
alejoviola <viola.alejo@outlook.com>
2021-10-18 11:58:59 -0300

    Adding React-Snap

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<head>
-			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-		</head>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+//Adding Hydrate for Twitter Card update
+import { hydrate } from 'react-dom';
+
+const rootElement = document.getElementById('root');
+if (rootElement?.hasChildNodes()) {
+	hydrate(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root'),
+	);
+} else {
+	ReactDOM.render(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root'),
+	);
+}
 
 reportWebVitals();
