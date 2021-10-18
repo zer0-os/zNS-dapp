4853352ddf448f7a6be0380d195ceff424fa87ca
alejoviola <viola.alejo@outlook.com>
2021-10-18 12:27:02 -0300

    Changing root render

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<head>
-			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-		</head>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+if (rootElement?.hasChildNodes()) {
+	ReactDOM.hydrate(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root'),
+	);
+} else {
+	ReactDOM.render(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root'),
+	);
+}
 
 reportWebVitals();
