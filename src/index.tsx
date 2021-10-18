4486ae8c165747749019d743e663aff107eec973
alejoviola <viola.alejo@outlook.com>
2021-10-18 17:47:30 -0300

    Revert "Adding React-Snap"
    This reverts commit 6ba06739f636591562d05e051f436c9734d8785b.

--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-if (rootElement?.hasChildNodes()) {
-	ReactDOM.hydrate(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		document.getElementById('root'),
-	);
-} else {
-	ReactDOM.render(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		document.getElementById('root'),
-	);
-}
+ReactDOM.render(
+	<React.StrictMode>
+		<head>
+			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+		</head>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
 
 reportWebVitals();
