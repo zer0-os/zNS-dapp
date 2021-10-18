83ba7b3bab5c786fdc8c9ceea4965b6c284c8de2
alejoviola <viola.alejo@outlook.com>
2021-10-18 16:30:45 -0300

    Revert "Adding React-Snap & Changing index"
    This reverts commit 1f0c7d6248b9c1aff86b3bdee55bb21d1a3500f9.

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { hydrate, render } from 'react-dom';
-
-const rootElement = document.getElementById('root');
-
-if (rootElement?.hasChildNodes()) {
-	hydrate(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		rootElement,
-	);
-} else {
-	render(
-		<React.StrictMode>
-			<head>
-				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-			</head>
-			<App />
-		</React.StrictMode>,
-		rootElement,
-	);
-}
+ReactDOM.render(
+	<React.StrictMode>
+		<head>
+			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+		</head>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
 
 reportWebVitals();
