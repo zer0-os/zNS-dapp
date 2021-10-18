d850c9fdc58afbbc284cb16291a6d5e6ce291cad
alejoviola <viola.alejo@outlook.com>
2021-10-18 20:17:54 -0300

    Testing Render

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from 'react-snapshot';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-render(
-	<React.StrictMode>
-		<head>
-			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-		</head>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+render(<App />, document.getElementById('root'));
 
 reportWebVitals();
