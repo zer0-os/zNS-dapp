9f3ca51f68a5ebbcb617699578840e75d3e4f9e0
alejoviola <viola.alejo@outlook.com>
2021-10-18 20:33:53 -0300

    Changing render

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from 'react-snapshot';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+		<head>
+			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+		</head>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
 
 reportWebVitals();
