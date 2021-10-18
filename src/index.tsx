3022f30867436c6cb454c7a736d4712731c5256b
alejoviola <viola.alejo@outlook.com>
2021-10-18 16:30:42 -0300

    Revert "Changing index.tsx"
    This reverts commit 8fcd4b8cfa8481077cca7b31bf47ddd9fc111c09.

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,28 @@ import reportWebVitals from './reportWebVitals';
 
 import { hydrate, render } from 'react-dom';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<head>
-			<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
-		</head>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement?.hasChildNodes()) {
+	hydrate(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		rootElement,
+	);
+} else {
+	render(
+		<React.StrictMode>
+			<head>
+				<meta name="fortmatic-site-verification" content="fdtdkNpVYri6vfLG" />
+			</head>
+			<App />
+		</React.StrictMode>,
+		rootElement,
+	);
+}
 
 reportWebVitals();
