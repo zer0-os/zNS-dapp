97f6ca36e01af11c991844640fa44c3d3f479464
Zachary <remscar@gmail.com>
2021-11-15 11:15:27 -0800

    Upgraded zAuction contract

--- src/lib/addresses.ts
+++ src/lib/addresses.ts
@@ -8,7 +8,7 @@ const addresses: { [network in NETWORK_TYPES]: ContractAddresses } = {
 		staking: '0x45b13d8e6579d5C3FeC14bB9998A3640CD4F008D',
 		wildToken: '0x2a3bFF78B79A009976EeA096a51A948a3dC00e34',
 		lootToken: '0x43b8219aC1883373C0428688eE1a76e19E6B6D9d',
-		zAuction: '0x05cBD37cA528B7ea50800aA80ddD0F9F30C952F0',
+		zAuction: '0x1aC7dE2b0776e0209c06Bc7A078D2a7950C2b7b9',
 		wheelSale: '0x19a55608f360f6Df69B7932dC2F65EDEFAa88Dc2',
 	},
 	[NETWORK_TYPES.KOVAN]: {
@@ -17,7 +17,7 @@ const addresses: { [network in NETWORK_TYPES]: ContractAddresses } = {
 		staking: '0x1E3F8B31b24EC0E938BE45ecF6971584F90A1602', //$LOOT staking controller
 		wildToken: '0x50A0A3E9873D7e7d306299a75Dc05bd3Ab2d251F',
 		lootToken: '0xD364C50c33902110230255FE1D730D84FA23e48e',
-		zAuction: '0x18A804a028aAf1F30082E91d2947734961Dd7f89',
+		zAuction: '0x1cb7330Cf59FEcb9e1502331fd7c835bA1bA9a66', // v2 deployment
 		wheelSale: '0xa6A3321b743C31912263090275E24d8b1A50cFE8', // wheelstest2
 	},
 } as any;
