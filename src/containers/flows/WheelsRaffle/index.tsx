f17366c43d97a010c2e6d082285822ed71b42d7b
Subrahmanyam Padala <subrahmanyam03@gmail.com>
2021-11-25 16:00:05 +0530

    feat(cribs-raffle): update the banner with actual timestamp

--- src/containers/flows/WheelsRaffle/index.tsx
+++ src/containers/flows/WheelsRaffle/index.tsx
@@ -13,14 +13,14 @@ const WheelsRaffleContainer = () => {
 	const currentTime = new Date().getTime();
 
 	// Temporary values
-	const RAFFLE_START_TIME = 1637829566370;
-	const RAFFLE_END_TIME = 1637829866370;
-	const SALE_START_TIME = 1637831066370;
+	// const RAFFLE_START_TIME = 1637829566370;
+	// const RAFFLE_END_TIME = 1637829866370;
+	// const SALE_START_TIME = 1637831066370;
 
 	// Hardcoded event times
-	// const RAFFLE_START_TIME = 1637870400000;
-	// const RAFFLE_END_TIME = 1638043200000;
-	// const SALE_START_TIME = 1638324000000;
+	const RAFFLE_START_TIME = 1637870400000;
+	const RAFFLE_END_TIME = 1638043200000;
+	const SALE_START_TIME = 1638324000000;
 
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
