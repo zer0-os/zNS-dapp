917e4c699ac01776e447099ab761d11d7df9bf44
paul <paul.oldenburger@springlane.de>
2021-11-30 01:03:16 +0100

    * feat: add ref forwarding
    * feat: add tooltip support

--- src/components/Buttons/FutureButton/FutureButton.tsx
+++ src/components/Buttons/FutureButton/FutureButton.tsx
@@ -1,5 +1,5 @@
 // React Imports
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 
 import styles from './FutureButtonStyle.module.scss';
 // Style Imports
@@ -14,6 +14,12 @@ type FutureButtonProps = {
 	glow?: boolean;
 	loading?: boolean;
 	alt?: boolean;
+	ref?: React.RefObject<any>;
+	onMouseEnter?: (e: React.MouseEvent) => void;
+	onMouseLeave?: (e: React.MouseEvent) => void;
+	onTouchStart?: (e: React.TouchEvent) => void;
+	onTouchMove?: (e: React.TouchEvent) => void;
+	onTouchEnd?: (e: React.TouchEvent) => void;
 };
 
 const cx = classNames.bind(styles);
@@ -25,61 +31,71 @@ export const TEST_ID = {
 };
 
 // @TODO Should make glow the default state since it's much more prevalent in the design
-const FutureButton: React.FC<FutureButtonProps> = ({
-	className,
-	onClick,
-	style,
-	toggleable,
-	children,
-	glow,
-	loading,
-	alt,
-}) => {
-	const [hasHovered, setHovered] = useState(false);
-	const [isSelected, setSelected] = useState(false);
+const FutureButton = forwardRef<HTMLButtonElement, FutureButtonProps>(
+	(
+		{
+			className,
+			onClick,
+			style,
+			toggleable,
+			children,
+			glow,
+			loading,
+			alt,
+			onMouseEnter,
+			...rest
+		},
+		ref,
+	) => {
+		const [hasHovered, setHovered] = useState(false);
+		const [isSelected, setSelected] = useState(false);
 
-	const handleHover = () => {
-		if (!glow) return;
-		else if (!hasHovered) setHovered(true);
-	};
+		const handleHover = (event: React.MouseEvent) => {
+			if (!glow) return;
+			else if (!hasHovered) setHovered(true);
+			if (onMouseEnter) onMouseEnter(event);
+		};
 
-	const handleClick = (event: any) => {
-		if (loading) return;
-		if (onClick) onClick(event);
-		if (toggleable) setSelected(!isSelected);
-	};
+		const handleClick = (event: any) => {
+			if (loading) return;
+			if (onClick) onClick(event);
+			if (toggleable) setSelected(!isSelected);
+		};
 
-	const buttonClasses = cx(className, {
-		futureButton: true,
-		futureButtonInactive: !glow,
-		glow: glow,
-		selected: isSelected,
-		Loading: loading,
-		Alt: alt,
-	});
+		const buttonClasses = cx(className, {
+			futureButton: true,
+			futureButtonInactive: !glow,
+			glow: glow,
+			selected: isSelected,
+			Loading: loading,
+			Alt: alt,
+		});
 
-	const washClasses = cx({
-		wash: true,
-		hovered: hasHovered && !isSelected,
-	});
+		const washClasses = cx({
+			wash: true,
+			hovered: hasHovered && !isSelected,
+		});
 
-	return (
-		<button
-			className={buttonClasses}
-			onMouseEnter={handleHover}
-			onMouseUp={handleClick}
-			style={style}
-			data-testid={TEST_ID.CONTAINER}
-		>
-			<div className={styles.Content}>
-				{!loading && children}
-				{loading && (
-					<div className={styles.Spinner} data-testid={TEST_ID.LOADER}></div>
-				)}
-			</div>
-			<div className={washClasses} data-testid={TEST_ID.WASH}></div>
-		</button>
-	);
-};
+		return (
+			<button
+				className={buttonClasses}
+				onMouseEnter={handleHover}
+				onMouseUp={handleClick}
+				ref={ref}
+				style={style}
+				data-testid={TEST_ID.CONTAINER}
+				{...rest}
+			>
+				<div className={styles.Content}>
+					{!loading && children}
+					{loading && (
+						<div className={styles.Spinner} data-testid={TEST_ID.LOADER}></div>
+					)}
+				</div>
+				<div className={washClasses} data-testid={TEST_ID.WASH}></div>
+			</button>
+		);
+	},
+);
 
 export default FutureButton;
