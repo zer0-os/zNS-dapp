e64803d16548f6f42ffcfe4d51c10906c2e13773
colbr <colbr243@gmail.com>
2021-12-12 21:27:33 +1300

    feat(stake-pool-table): remove pool data from helper Pool data now comes from useStaking hook

--- src/containers/staking/StakePoolTable/StakePoolTable.helpers.tsx
+++ src/containers/staking/StakePoolTable/StakePoolTable.helpers.tsx
@@ -1,6 +1,3 @@
-import stakeIcon from './assets/stake.png';
-import farmIcon from './assets/farm.png';
-
 export const TABLE_HEADERS = [
 	{
 		label: '',
@@ -42,30 +39,5 @@ export const TABLE_HEADERS = [
 		label: '',
 		accessor: '',
 		className: '',
-	},
-];
-
-export const POOL_DATA = [
-	{
-		apy: 1002,
-		domain: 'wilder.stakewild',
-		id: '1',
-		image: stakeIcon,
-		name: 'Stake Wild',
-		numStakers: 125215,
-		token: 'WILD',
-		totalRewardsIssued: 52185921,
-		tvl: 1005525,
-	},
-	{
-		apy: 1002,
-		domain: 'wilder.farmwild',
-		id: '2',
-		image: farmIcon,
-		name: 'Farm WILD - WETH LP',
-		numStakers: 125215,
-		token: 'LP',
-		totalRewardsIssued: 52185921,
-		tvl: 1005525,
 	},
 ];
