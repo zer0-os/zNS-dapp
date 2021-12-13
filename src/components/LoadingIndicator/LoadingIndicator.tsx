811f4c5d65e174b1412f74948706e659c67d2974
colbr <colbr243@gmail.com>
2021-12-14 10:49:41 +1300

    feat(loading-indicator): allow ReactNode as content Can now pass ReactNodes as children so we can display links, buttons, multiple paragraphs, etc

--- src/components/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,19 +1,18 @@
 import { Spinner } from 'components';
+import React from 'react';
 
 import styles from './LoadingIndicator.module.scss';
 
 type LoadingIndicatorProps = {
 	style?: React.CSSProperties;
-	text: string;
+	text: string | React.ReactNode;
 };
 
-const LoadingIndicator = (props: LoadingIndicatorProps) => {
-	return (
-		<div style={props.style} className={styles.Container}>
-			<p>{props.text}</p>
-			<Spinner />
-		</div>
-	);
-};
+const LoadingIndicator = (props: LoadingIndicatorProps) => (
+	<div style={props.style} className={styles.Container}>
+		<p>{props.text}</p>
+		<Spinner />
+	</div>
+);
 
 export default LoadingIndicator;
