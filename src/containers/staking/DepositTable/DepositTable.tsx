500c0e24ab5afdbf934764e14105dd0020c6adcb
colbr <colbr243@gmail.com>
2021-12-14 23:58:20 +1300

    feat(deposit-table): handle no wallet connected & no deposits

--- src/containers/staking/DepositTable/DepositTable.tsx
+++ src/containers/staking/DepositTable/DepositTable.tsx
@@ -6,20 +6,43 @@ import { useStaking } from 'lib/providers/staking/StakingProvider';
 import styles from './DepositTable.module.scss';
 
 import { TABLE_HEADERS } from './DepositTable.helpers';
+import { useWeb3React } from '@web3-react/core';
+import { ConnectWalletButton } from 'containers';
+import { Link } from 'react-router-dom';
 
 const DepositTable = () => {
 	const { deposits } = useStaking();
+	const { active } = useWeb3React();
+
+	if (!active) {
+		return (
+			<div className={styles.Container}>
+				<p>Connect a Web3 wallet to see your Staking data</p>
+				<ConnectWalletButton>Connect Wallet</ConnectWalletButton>
+			</div>
+		);
+	} else if (!deposits || (deposits && deposits.length)) {
+		return (
+			<GenericTable
+				alignments={[0, 0, 1, 1, 1, 1, 1]}
+				data={deposits}
+				isLoading={deposits === undefined}
+				loadingText={'Loading Deposits'}
+				itemKey={'id'}
+				headers={TABLE_HEADERS}
+				empty={deposits?.length === 0}
+				rowComponent={DepositTableRow}
+			/>
+		);
+	}
 
 	return (
-		<GenericTable
-			alignments={[0, 0, 1, 1, 1, 1, 1]}
-			className={styles.Table}
-			data={deposits}
-			isLoading={deposits === undefined}
-			itemKey={'id'}
-			headers={TABLE_HEADERS}
-			rowComponent={DepositTableRow}
-		/>
+		<div className={styles.Container}>
+			<p>
+				You have not staked in any pools. Get started on the{' '}
+				<Link to={'/pools'}>Pools Page</Link>
+			</p>
+		</div>
 	);
 };
 
