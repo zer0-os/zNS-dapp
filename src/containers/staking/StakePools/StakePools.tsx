58f37d4f4f092ee55409ec070e56fee1080a6f94
colbr <colbr243@gmail.com>
2021-12-17 23:01:24 +1300

    feat(staking): remove global stats These are currently not available in the SDK

--- src/containers/staking/StakePools/StakePools.tsx
+++ src/containers/staking/StakePools/StakePools.tsx
@@ -1,51 +1,11 @@
-import { StatsWidget } from 'components';
 import { StakePoolTable } from 'containers/staking';
-import { useEffect } from 'react';
-import styles from './StakePools.module.scss';
-
-import { useStaking } from 'lib/providers/staking/StakingSDKProvider';
 
 const StakePools = () => {
-	const { pools } = useStaking();
+	// This file looks empty because we used to
+	// have some StatWidets in here - if we need to add
+	// them back, put them in a <ul> here
 
-	useEffect(() => {
-		let isMounted = true;
-		return () => {
-			isMounted = false;
-		};
-	}, []);
-
-	return (
-		<>
-			<ul className={styles.Stats}>
-				<StatsWidget
-					className="normalView"
-					fieldName={'Total Amount Staked'}
-					isLoading={false}
-					title={'-'}
-				/>
-				<StatsWidget
-					className="normalView"
-					fieldName={'Rewards Issued'}
-					isLoading={false}
-					title={'-'}
-				/>
-				<StatsWidget
-					className="normalView"
-					fieldName={'# Of People Staking'}
-					isLoading={false}
-					title={'-'}
-				/>
-				<StatsWidget
-					className="normalView"
-					fieldName={'# Of Pools'}
-					isLoading={pools === undefined}
-					title={2}
-				/>
-			</ul>
-			<StakePoolTable />
-		</>
-	);
+	return <StakePoolTable />;
 };
 
 export default StakePools;
