f436230e58b9a5e259d5fc681a963c74af5b5b06
JeffersonMat <jeff_08062011@hotmail.com>
2021-12-19 11:54:36 +1300

    removed cahnge to pkg json test

--- src/components/Artwork/Artwork.test.tsx
+++ src/components/Artwork/Artwork.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Artwork, { TEST_ID } from './Artwork';
 import { BrowserRouter } from 'react-router-dom';
