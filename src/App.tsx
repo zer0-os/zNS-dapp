eedeb45788b1feb61513a7f476ee12601095c8b3
colbr <colbr243@gmail.com>
2021-12-22 11:25:23 +1300

    refactor(App): clean up file

--- src/App.tsx
+++ src/App.tsx
@@ -12,32 +12,20 @@ import store, { history } from './store';
 import 'styles/reset.scss';
 import 'styles/main.scss';
 
-//- React Imports
-import { HashRouter, Route } from 'react-router-dom';
-
 //- Web3 Imports
-import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
+import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 
 //- Library Imports
 import CacheBuster from 'react-cache-buster';
-import MintProvider from 'lib/providers/MintProvider';
-import BidProvider from 'lib/providers/BidProvider';
-import EnlistProvider from 'lib/providers/EnlistProvider';
-import TransferProvider from './lib/providers/TransferProvider';
 import { ChainSelectorProvider } from 'lib/providers/ChainSelectorProvider';
 import { SubgraphProvider } from 'lib/providers/SubgraphProvider';
-import CurrentDomainProvider from 'lib/providers/CurrentDomainProvider';
 
 //- Asset Imports
 import backgroundImage from 'assets/background.jpg';
 
 //- Page Imports
 import { Navigator } from 'containers';
-import { ZNS } from 'pages';
-import StakingRequestProvider from 'lib/providers/StakingRequestProvider';
-import { ZNSDomainsProvider } from 'lib/providers/ZNSDomainProvider';
-import { network } from 'lib/connectors';
 
 // Web3 library to query
 function getLibrary(provider: any): Web3Provider {
@@ -69,19 +57,7 @@ function App() {
 
 	return (
 		<ConnectedRouter history={history}>
-			{/* <HashRouter>
-				<Route
-					render={({ location, match }) => {
-						return (
-							<> */}
-			{/* <CurrentDomainProvider> */}
 			<Navigator domain={''} />
-			{/* </CurrentDomainProvider> */}
-			{/* </>
-						);
-					}}
-				/>
-			</HashRouter> */}
 		</ConnectedRouter>
 	);
 }
@@ -100,19 +76,7 @@ function wrappedApp() {
 					<SubgraphProvider>
 						<Web3ReactProvider getLibrary={getLibrary}>
 							{/* Our Hooks  */}
-							{/* <ZNSDomainsProvider>
-								<BidProvider>
-									<TransferProvider>
-										<StakingRequestProvider>
-											<MintProvider>
-												<EnlistProvider> */}
 							<App />
-							{/* </EnlistProvider>
-											</MintProvider>
-										</StakingRequestProvider>
-									</TransferProvider>
-								</BidProvider>
-							</ZNSDomainsProvider> */}
 						</Web3ReactProvider>
 					</SubgraphProvider>
 				</ChainSelectorProvider>
