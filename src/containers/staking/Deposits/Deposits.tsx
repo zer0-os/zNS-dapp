b87271b83747d4003ced2f56e2ea2847e68f309c
Brett <colbr243@gmail.com>
2022-01-20 11:38:32 +1300

    Release 0.2.0 (#351)
    * build: upgrade zFI-SDK version
    
    * build: npm install
    
    * feat(staking-deposits): fetch user value staked through SDK
    This is currently broken, just pushing for protocol team to check
    
    * build: upgrade zfi-sdk version
    
    * build: npm i
    
    * feat(deposits): show total staked USD value
    
    * refactor(deposits): readability and efficiency
    Split up the code and commented for readability
    Made all data retrieval run non-blocking
    
    * build: increment version number
    
    * bug(currency): fix underflow/overflow issues
    Fixed some odd logic where BigNumbers were being used incorrectly

--- src/containers/staking/Deposits/Deposits.tsx
+++ src/containers/staking/Deposits/Deposits.tsx
@@ -1,59 +1,90 @@
+// React imports
+import { useEffect, useState } from 'react';
+
+// Web3 imports
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
-import { StatsWidget } from 'components';
-import { DepositTable } from 'containers/staking';
 import { ethers } from 'ethers';
+
+// Library imports
 import { useZnsContracts } from 'lib/contracts';
 import { displayEther, displayEtherToFiat, toFiat } from 'lib/currency';
 import useCurrency from 'lib/hooks/useCurrency';
 import { useStaking } from 'lib/providers/staking/StakingSDKProvider';
 import { MaybeUndefined } from 'lib/types';
-import { useEffect, useState } from 'react';
+
+// Component & container imports
+import { StatsWidget } from 'components';
+import { DepositTable } from 'containers/staking';
+
+// Style imports
 import styles from './Deposits.module.scss';
 
 const Deposits = () => {
+	// Relevant hooks
 	const staking = useStaking();
 	const contracts = useZnsContracts();
 	const { active, account } = useWeb3React<Web3Provider>();
 	const { wildPriceUsd } = useCurrency();
 
+	// User data
 	const [totalRewardsClaimable, setTotalRewardsClaimable] =
 		useState<MaybeUndefined<ethers.BigNumber>>();
-
 	const [wildBalance, setWildBalance] =
 		useState<MaybeUndefined<ethers.BigNumber>>();
+	const [totalStakedUsd, setTotalStakedUsd] =
+		useState<MaybeUndefined<number>>();
 
+	// On load, get user data and store in state
 	useEffect(() => {
 		let isMounted = true;
 
 		setWildBalance(undefined);
+		setTotalStakedUsd(undefined);
 		setTotalRewardsClaimable(undefined);
 
-		const getMetrics = async () => {
+		// Gets all user metrics: wild balance, claimable rewards, total stake (usd)
+		(async () => {
 			if (!account || !staking.pools) {
 				return;
 			}
 
-			const lpReward = await staking.pools.lpPool.instance.pendingYieldRewards(
-				account,
-			);
-			const wildReward =
-				await staking.pools.wildPool.instance.pendingYieldRewards(account);
+			// Gets user's WILD balance
+			contracts!.wildToken.balanceOf(account).then((balance) => {
+				setWildBalance(balance);
+			});
 
-			const totalReward = lpReward.add(wildReward);
+			// Gets total claimable rewards
+			Promise.all([
+				staking.pools.lpPool.instance.pendingYieldRewards(account),
+				staking.pools.wildPool.instance.pendingYieldRewards(account),
+			]).then(([lpRewards, wildRewards]) => {
+				if (!isMounted || !lpRewards || !wildRewards) {
+					// error handling
+					return;
+				}
+				setTotalRewardsClaimable(lpRewards.add(wildRewards));
+			});
 
-			if (isMounted) {
-				setTotalRewardsClaimable(totalReward);
-			}
-
-			const wildTokenBalance = await contracts!.wildToken.balanceOf(account);
-			setWildBalance(wildTokenBalance);
-		};
-		getMetrics();
+			// Gets total user stake in USD
+			Promise.all([
+				staking.instance?.liquidityPool.userValueStaked(account),
+				staking.instance?.wildPool.userValueStaked(account),
+			]).then(([lpStake, wildStake]) => {
+				if (!lpStake || !wildStake || !isMounted) {
+					// error handling
+					return;
+				}
+				setTotalStakedUsd(
+					lpStake.userValueUnlockedUsd + wildStake.userValueUnlockedUsd,
+				);
+			});
+		})();
 
 		return () => {
 			isMounted = false;
 		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [account, staking.pools]);
 
 	return (
@@ -88,6 +119,16 @@ const Deposits = () => {
 							totalRewardsClaimable &&
 							wildPriceUsd &&
 							'$' + displayEtherToFiat(totalRewardsClaimable, wildPriceUsd)
+						}
+					/>
+					<StatsWidget
+						className="normalView"
+						fieldName={'Your Total Stake (USD)'}
+						isLoading={account !== null && !totalStakedUsd}
+						title={
+							account && totalStakedUsd !== undefined
+								? '$' + toFiat(totalStakedUsd) + ' USD'
+								: '-'
 						}
 					/>
 				</ul>
