28327a719fd1acf5a75592a5049454ff78916386
JeffersonMat <jeff_08062011@hotmail.com>
2022-02-02 21:09:34 +1300

    converted TextButton component to tsx and added test for it

---
+++ src/components/Buttons/TextButton/TextButton.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import styles from './TextButton.module.scss';
+
+export const TEST_ID = {
+	BUTTON: 'text-button',
+};
+type TextButtonProps = {
+	className?: string;
+	onClick?: () => any;
+	style?: React.CSSProperties;
+	toggleable?: boolean;
+	selected?: boolean;
+	children?: React.ReactNode;
+};
+const TextButton: React.FC<TextButtonProps> = ({
+	className,
+	onClick,
+	style,
+	toggleable,
+	selected,
+	children,
+}) => {
+	const [isSelected, setIsSelected] = useState(false);
+
+	const handleClick = () => {
+		setIsSelected(!isSelected);
+		if (onClick) onClick();
+	};
+
+	return (
+		<button
+			onClick={handleClick}
+			className={`${styles.textButton} ${
+				(toggleable && isSelected) || selected ? styles.selected : ''
+			} ${className || ''}`}
+			style={style}
+			data-testid={TEST_ID.BUTTON}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default TextButton;
