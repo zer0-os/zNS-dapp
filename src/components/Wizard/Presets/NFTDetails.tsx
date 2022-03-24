be291182232c8d16e16c07fa9ce20199581c4d8c
dominic <domw30@gmail.com>
2022-03-24 20:00:04 +0000

    allow for truncated domain in Wizard NFTDetails with max length set

--- src/components/Wizard/Presets/NFTDetails.tsx
+++ src/components/Wizard/Presets/NFTDetails.tsx
@@ -1,6 +1,9 @@
 import styles from './NFTDetails.module.scss';
 import classNames from 'classnames';
 import { Detail as DetailComponent, Member, NFTMedia } from 'components';
+import { truncateDomain } from 'lib/utils';
+
+const maxCharacterLength = 27;
 
 type Detail = {
 	name: string;
@@ -23,31 +26,35 @@ const NFTDetails = ({
 	domain,
 	title,
 	otherDetails,
-}: NFTDetailsProps) => (
-	<div className={classNames(styles.Container, className)}>
-		<NFTMedia
-			alt="NFT Preview"
-			className={classNames(styles.Image, 'img-border-rounded')}
-			disableLightbox
-			fit="cover"
-			ipfsUrl={assetUrl}
-			size="small"
-		/>
-		<div className={styles.Details}>
-			<div>
-				<h1 className="glow-text-white">{title}</h1>
-				<h2>0://{domain}</h2>
+}: NFTDetailsProps) => {
+	// Truncate domain
+	const formattedDomain = truncateDomain(domain, maxCharacterLength);
+	return (
+		<div className={classNames(styles.Container, className)}>
+			<NFTMedia
+				alt="NFT Preview"
+				className={classNames(styles.Image, 'img-border-rounded')}
+				disableLightbox
+				fit="cover"
+				ipfsUrl={assetUrl}
+				size="small"
+			/>
+			<div className={styles.Details}>
+				<div>
+					<h1 className="glow-text-white">{title}</h1>
+					<h2>0://{formattedDomain}</h2>
+				</div>
+				{creator && <Member id={creator} subtext={'Creator'} />}
+				{otherDetails?.map((detail: Detail, i) => (
+					<DetailComponent
+						key={i + detail.value}
+						text={detail.value}
+						subtext={detail.name}
+					/>
+				))}
 			</div>
-			{creator && <Member id={creator} subtext={'Creator'} />}
-			{otherDetails?.map((detail: Detail, i) => (
-				<DetailComponent
-					key={i + detail.value}
-					text={detail.value}
-					subtext={detail.name}
-				/>
-			))}
 		</div>
-	</div>
-);
+	);
+};
 
 export default NFTDetails;
