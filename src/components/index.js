d700bcaa12b76d8af1bc078c520a28798d61ef04
dominic <domw30@gmail.com>
2022-03-28 07:54:20 +0100

    remove request table from components index file

--- src/components/index.js
+++ src/components/index.js
@@ -47,7 +47,6 @@ export { default as TextInputWithTopPlaceHolder } from './Inputs/TextInput/TextI
 export { default as TabBar } from './NavBars/TabBar/TabBar';
 
 //- Tables
-export { default as RequestTable } from './Tables/RequestTable/RequestTable';
 export { default as GenericTable } from './Tables/GenericTable/GenericTable';
 
 //- Other
