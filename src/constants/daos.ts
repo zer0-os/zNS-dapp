9c2b2e741c13128d64ecf74094eba850e18f6e70
Brett <12437916+colbr@users.noreply.github.com>
2022-03-30 02:26:10 +1300

    feat(zdao): add test DAOs for mainnet

--- src/constants/daos.ts
+++ src/constants/daos.ts
@@ -3,14 +3,22 @@ import { NETWORK_TYPES } from 'lib/network';
 
 export const DAOS: { [network in NETWORK_TYPES]: CreateZDAOParams[] } = {
 	[NETWORK_TYPES.MAINNET]: [
-		// {
-		// 	zNA: 'real',
-		// 	title: 'zDAO Testing 1',
-		// 	creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
-		// 	network: 1, // for Rinkeby
-		// 	safeAddress: '0x9CA53E3E249Abe500bEbA7aC8BDC8476bfc06bC6',
-		// 	votingToken: '0x10F6A2795B14f13771d885D72e5925Aff647B565',
-		// },
+		{
+			zNA: 'adao',
+			title: 'Mainnet Test DAO 1',
+			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
+			network: 1, // for Rinkeby
+			safeAddress: '0x9CA53E3E249Abe500bEbA7aC8BDC8476bfc06bC6',
+			votingToken: '0x10F6A2795B14f13771d885D72e5925Aff647B565',
+		},
+		{
+			zNA: 'anotherone',
+			title: 'Mainnet Test DAO 2',
+			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
+			network: 4, // for Rinkeby
+			safeAddress: '0x7a935d07d097146f143A45aA79FD8624353abD5D',
+			votingToken: '0x10F6A2795B14f13771d885D72e5925Aff647B565',
+		},
 	],
 	[NETWORK_TYPES.RINKEBY]: [
 		{
