75da80deabb4096dfb398584f6d2886a56bcdd11
eric-zero-dev <91501349+eric-zero-dev@users.noreply.github.com>
2022-04-08 12:48:09 +0100

    fix: hash parsing from IPFS url (#492)
    * Fix hash parsing from IPFS url
    
    * Update ipfs.ts
    
    Co-authored-by: Brett <colbr243@gmail.com>

--- src/lib/ipfs.ts
+++ src/lib/ipfs.ts
@@ -2,13 +2,18 @@ import { DEFAULT_IPFS_GATEWAY } from 'constants/ipfs';
 
 /**
  * Pulls the IPFS hash from an IPFS url
- * Note: this will currently not handle nested IPFS content, i.e. ipfs.io/ipfs/[hash]/content
  * @param url IPFS url to get hash from
  * @returns IPFS hash from url
  */
 export const getHashFromIPFSUrl = (url: string) => {
-	const hashIndex = url.lastIndexOf('/') + 1;
-	return url.slice(hashIndex);
+	const regex = /Qm[a-zA-Z0-9/]*/;
+
+	if (regex.test(url)) {
+		const matches = url.match(regex) as string[];
+		return matches[0];
+	}
+
+	return '';
 };
 
 export const getWebIPFSUrlFromHash = (hash: string) => {
