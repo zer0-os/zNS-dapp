df534ad0c8467308e37c334208a0d30418437929
Dom <domw30@gmail.com>
2022-04-08 06:46:40 +0100

    hotfix: Filter Bids in Bid List when used can Accept Bids - Display all by default (#483)
    * fix: show all bids by default in bid list - add condition for isAcceptBidEnabled
    
    * refactor: minor change - when to filter bids

--- src/containers/Tables/OwnedDomainsTable/OwnedDomainsTableRow.tsx
+++ src/containers/Tables/OwnedDomainsTable/OwnedDomainsTableRow.tsx
@@ -94,6 +94,7 @@ const OwnedDomainsTableRow = ({
 						onAccept={refetch}
 						isLoading={isLoadingBidData}
 						highestBid={highestBid?.amount}
+						isAcceptBidEnabled
 					/>
 				</Overlay>
 			);
