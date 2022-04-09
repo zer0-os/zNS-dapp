39e164f226332dffad13c4d920e57008d13c3dff
Brett <12437916+colbr@users.noreply.github.com>
2022-04-09 20:53:23 +1200

    test: add tests and comments for currency utils

---
+++ src/lib/currency.test.ts
@@ -0,0 +1,80 @@
+import { parseEther } from '@ethersproject/units';
+import { BigNumber } from 'ethers';
+import { displayEtherToFiat, toFiat, displayEther } from './currency';
+
+//////////////////////
+// f:: displayEther //
+//////////////////////
+
+describe('displayEther', () => {
+	it('should convert to locale string', () => {
+		expect(displayEther(BigNumber.from(parseEther('1000')))).toBe('1,000.00');
+	});
+
+	it('should apply default 2 dp', () => {
+		expect(displayEther(BigNumber.from(parseEther('10')))).toBe('10.00');
+	});
+
+	it('should handle specified dp', () => {
+		expect(displayEther(BigNumber.from(parseEther('10')), 3)).toBe('10.000');
+	});
+
+	it('should handle zero', () => {
+		expect(displayEther(BigNumber.from(parseEther('0')))).toBe('0.00');
+	});
+
+	it('should handle negative numbers', () => {
+		expect(displayEther(BigNumber.from(parseEther('-10')))).toBe('-10.00');
+	});
+});
+
+////////////////
+// f:: toFiat //
+////////////////
+
+describe('toFiat', () => {
+	it('should convert to locale string', () => {
+		expect(toFiat(1000)).toBe('1,000.00');
+	});
+
+	it('should handle zero', () => {
+		expect(toFiat(0)).toBe('0.00');
+	});
+
+	it('should handle negative numbers', () => {
+		expect(toFiat(-10)).toBe('-10.00');
+	});
+
+	it('should handle NaN', () => {
+		expect(toFiat(NaN)).toBe(undefined);
+	});
+});
+
+////////////////////////////
+// f:: displayEtherToFiat //
+////////////////////////////
+
+describe('displayEtherToFiat', () => {
+	const defaultEtherAmount = BigNumber.from(parseEther('100'));
+
+	it('should apply 2dp', () => {
+		expect(displayEtherToFiat(defaultEtherAmount, 1)).toBe('100.00');
+	});
+
+	it('should multiply number by conversion rate', () => {
+		expect(displayEtherToFiat(defaultEtherAmount, 5)).toBe('500.00');
+	});
+
+	it('should convert to locale string', () => {
+		expect(displayEtherToFiat(defaultEtherAmount, 10)).toBe('1,000.00');
+	});
+
+	it('should handle zero', () => {
+		expect(displayEtherToFiat(BigNumber.from(0), 10)).toBe('0.00');
+	});
+
+	it('should handle negatives', () => {
+		const negative = BigNumber.from(parseEther('-1'));
+		expect(displayEtherToFiat(negative, 10)).toBe('-10.00');
+	});
+});
--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -1,24 +1,46 @@
 import { ethers } from 'ethers';
 
-export const toFiat = (x: number) =>
-	x.toLocaleString(undefined, {
-		maximumFractionDigits: 2,
-		minimumFractionDigits: 2,
-	});
+/**
+ * Converts a number to fiat format - just a locale string
+ * with 2 dp
+ * @param n number to convert
+ * @returns number as locale string converted to 2dp
+ */
+export const toFiat = (n: number) => {
+	if (!isNaN(n)) {
+		return n.toLocaleString(undefined, {
+			maximumFractionDigits: 2,
+			minimumFractionDigits: 2,
+		});
+	}
+};
 
-export const displayEther = (x: ethers.BigNumber, decimalPlaces?: number) => {
-	return Number(Number(ethers.utils.formatEther(x))).toLocaleString(undefined, {
+/**
+ * Converts a big number to a locale string with decimals
+ * @param n number to convert
+ * @param decimalPlaces decimal places to round to (default 2)
+ * @returns a converted big number
+ */
+export const displayEther = (n: ethers.BigNumber, decimalPlaces?: number) => {
+	return Number(Number(ethers.utils.formatEther(n))).toLocaleString(undefined, {
 		maximumFractionDigits: decimalPlaces || 2,
 		minimumFractionDigits: decimalPlaces || 2,
 	});
 };
 
+/**
+ * Multiplies a BigNumber by a conversion rate, and
+ * returns it in Fiat format (locale string with 2dp)
+ * @param n number to convert
+ * @param conversionRate conversion rate to apply
+ * @returns fiat format of BigNumber * conversion rate
+ */
 export const displayEtherToFiat = (
-	x: ethers.BigNumber,
+	n: ethers.BigNumber,
 	conversionRate: number,
 ) => {
 	try {
-		const fiat = toFiat(Number(ethers.utils.formatEther(x)) * conversionRate);
+		const fiat = toFiat(Number(ethers.utils.formatEther(n)) * conversionRate);
 		return fiat;
 	} catch {
 		return '-';
