07fcc6d65aa7041c6c3164586c403a858b535b8a
Brett <colbr243@gmail.com>
2022-04-17 23:00:44 +0000

    test: Increase coverage (#516)
    * chore: bump version number
    
    * fix: hash parsing from IPFS url (#492)
    
    * Fix hash parsing from IPFS url
    
    * Update ipfs.ts
    
    Co-authored-by: Brett <colbr243@gmail.com>
    
    * fix: force better screen rendering (#493)
    
    * test: add tests and comments for currency utils
    
    * test(ipfs): test ipfs utils
    
    * Merge branch 'develop' into test/increasing-coverage
    
    * test(metadata): added tests for metadata utils
    
    * fix(currency): toFiat shouldn't return undefined
    
    * Fix compile issue
    
    * Fix hash parsing from IPFS url and updated the test
    
    * Fix hash pasing with CIDv0 format and length, and add test
    
    * Fix currency tests
    
    * Fix metadata test to use correct CIDv0 format hash
    
    Co-authored-by: eric-zero-dev <91501349+eric-zero-dev@users.noreply.github.com>
    Co-authored-by: ericlow <eric.low.dev@gmail.com>

---
+++ src/lib/ipfs.test.ts
@@ -0,0 +1,68 @@
+import { getHashFromIPFSUrl, getWebIPFSUrlFromHash } from './ipfs';
+import { DEFAULT_IPFS_GATEWAY } from 'constants/ipfs';
+
+////////////////////////////
+// f:: getHashFromIPFSUrl //
+////////////////////////////
+
+// Hashes all follow the format Qm
+export const HASH = 'QmXuTHUeqMqABWTcX8mYVMtH4sG3nuTxTzZL8t6zSLZVxN';
+
+const nesting = [...Array(10)]
+	.map(() => (Math.random() + 1).toString(36).substring(7))
+	.join('/');
+
+describe('getHashFromIPFSUrl', () => {
+	it('should handle ipfs:// URLs', () => {
+		expect(getHashFromIPFSUrl('ipfs://' + HASH)).toBe(HASH);
+	});
+
+	it('should handle arbitrary web URLs', () => {
+		expect(getHashFromIPFSUrl('https://ipfs.fleek.co/ipfs/' + HASH)).toBe(HASH);
+		expect(getHashFromIPFSUrl('https://ipfs.io/ipfs/' + HASH)).toBe(HASH);
+		expect(getHashFromIPFSUrl('test.com/' + HASH)).toBe(HASH);
+		expect(getHashFromIPFSUrl('192.168.1.1/' + HASH)).toBe(HASH);
+	});
+
+	it('should handle deeply nested URLs', () => {
+		const url = `https://test.com/${nesting}/${HASH}`;
+		expect(getHashFromIPFSUrl(url)).toBe(HASH);
+	});
+
+	it('should handle URLs with no IPFS hash', () => {
+		expect(getHashFromIPFSUrl('https://ipfs.fleek.co/')).toBe('');
+	});
+
+	it('should handle nested IPFS content', () => {
+		const WEB_URL = `https://test.com/`;
+		expect(getHashFromIPFSUrl(`${WEB_URL}/${HASH}/${nesting}`)).toBe(
+			`${HASH}/${nesting}`,
+		);
+		expect(getHashFromIPFSUrl(`${`${WEB_URL}/${HASH}/4`}`)).toBe(`${HASH}/4`);
+	});
+
+	it('should handle URLs containing Qm', () => {
+		expect(getHashFromIPFSUrl('test.com/Qm/' + HASH)).toBe(HASH);
+		expect(getHashFromIPFSUrl('Qm.com/' + HASH)).toBe(HASH);
+		expect(getHashFromIPFSUrl('test.com/' + HASH + '/Qm')).toBe(HASH + '/Qm');
+		expect(getHashFromIPFSUrl('ipfs.com/Qmg/' + HASH + '/test')).toBe(
+			HASH + '/test',
+		);
+	});
+
+	// CIDv0 is CID which has 46 characters starting with "Qm"
+	// More info - https://docs.ipfs.io/concepts/content-addressing/#cid-conversion
+	it('should handle URLs containing Qm but invalid CIDv0 length', () => {
+		const invalidCID = HASH.slice(0, 42);
+		expect(getHashFromIPFSUrl('test.com/Qm/' + invalidCID)).toBe('');
+		expect(getHashFromIPFSUrl('Qm.com/' + invalidCID)).toBe('');
+		expect(getHashFromIPFSUrl('test.com/' + invalidCID + '/Qm')).toBe('');
+		expect(getHashFromIPFSUrl('ipfs.com/Qmg/' + invalidCID + '/test')).toBe('');
+	});
+});
+
+describe('getWebIPFSUrlFromHash', () => {
+	it('should append hash to default IPFS gateway URL', () => {
+		expect(getWebIPFSUrlFromHash(HASH)).toBe(DEFAULT_IPFS_GATEWAY + HASH);
+	});
+});
