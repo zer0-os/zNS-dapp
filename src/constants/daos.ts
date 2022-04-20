dd1165ac86a47b4d91b1f949b82832bddf1bb6cb
Brett <colbr243@gmail.com>
2022-04-21 10:08:57 +1200

    feat: re-order DAOs to be alphabetical

--- src/constants/daos.ts
+++ src/constants/daos.ts
@@ -4,6 +4,14 @@ import { NETWORK_TYPES } from 'lib/network';
 export const DAOS: { [network in NETWORK_TYPES]: CreateZDAOParams[] } = {
 	[NETWORK_TYPES.MAINNET]: [
 		{
+			zNA: 'wilder.beasts',
+			title: 'Beasts DAO',
+			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
+			network: 1, // for Mainnet
+			safeAddress: '0x766a9b866930d0c7f673eb8fc9655d5f782b2b21',
+			votingToken: '0x2a3bFF78B79A009976EeA096a51A948a3dC00e34',
+		},
+		{
 			zNA: 'wilder.kicks',
 			title: 'Kicks DAO',
 			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
@@ -17,14 +25,6 @@ export const DAOS: { [network in NETWORK_TYPES]: CreateZDAOParams[] } = {
 			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
 			network: 1, // for Mainnet
 			safeAddress: '0xEe7Ad892Fdf8d95223d7E94E4fF42E9d0cfeCAFA',
-			votingToken: '0x2a3bFF78B79A009976EeA096a51A948a3dC00e34',
-		},
-		{
-			zNA: 'wilder.beasts',
-			title: 'Beasts DAO',
-			creator: '0x22C38E74B8C0D1AAB147550BcFfcC8AC544E0D8C',
-			network: 1, // for Mainnet
-			safeAddress: '0x766a9b866930d0c7f673eb8fc9655d5f782b2b21',
 			votingToken: '0x2a3bFF78B79A009976EeA096a51A948a3dC00e34',
 		},
 	],
