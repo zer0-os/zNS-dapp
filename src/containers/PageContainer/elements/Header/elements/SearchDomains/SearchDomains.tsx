6cb8669bdb09b94062902d437b34392590060f99
dominic <domw30@gmail.com>
2022-05-02 22:21:52 +0100

    fix: add wilder to search results

--- src/containers/PageContainer/elements/Header/elements/SearchDomains/SearchDomains.tsx
+++ src/containers/PageContainer/elements/Header/elements/SearchDomains/SearchDomains.tsx
@@ -2,7 +2,10 @@ import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Spring, animated } from 'react-spring';
 import { useDomainSearch } from 'lib/useDomainSearch';
-import { SEARCH_NOT_FOUND } from './SearchDomains.constants';
+import {
+	SEARCH_NOT_FOUND,
+	IS_EXACT_MATCH_ENABLED,
+} from './SearchDomains.constants';
 import {
 	useSearchDomainsData,
 	useSearchDomainsHandlers,
@@ -50,6 +53,18 @@ export const SearchDomains: React.FC<SearchDomainsProps> = ({
 				>
 					<div className="search-domains__results-content">
 						<ul ref={listRef}>
+							{IS_EXACT_MATCH_ENABLED && domainSearch?.exactMatch?.name && (
+								<li
+									className="exact__match"
+									key={domainSearch.exactMatch.name}
+									onMouseDown={handlers.handleDomainClick(
+										domainSearch?.exactMatch?.name || '',
+									)}
+								>
+									{getLastDomainName(domainSearch.exactMatch.name)}{' '}
+									<span>{domainSearch.exactMatch.name}</span>
+								</li>
+							)}
 							{domainSearch?.matches
 								?.filter((d) => d.name.length > 1)
 								.map((s, i) => (
