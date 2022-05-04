30f72da01b1bdd79fef691ec46859e17091cfaf0
Brett <colbr243@gmail.com>
2022-05-04 05:41:53 +0000

    feat(side-bar): sidebar logo should change depending on world (#560)
    Zero logo at bottom of sidebar should also show/hide depending on zNA

--- src/constants/domains.ts
+++ src/constants/domains.ts
@@ -5,3 +5,14 @@
  *  empty string for zero.live app
  */
 export const ROOT_DOMAIN = '';
+
+/**
+ * Hardcoded domain logos
+ * These will need to be pulled in dynamically
+ * at some point.
+ */
+export const DOMAIN_LOGOS = {
+	WILDER_WORLD:
+		'https://res.cloudinary.com/fact0ry/image/upload/c_fill,h_32,q_100,w_32/logos/wilderworld.svg',
+	ZERO: 'https://res.cloudinary.com/fact0ry/image/upload/c_fill,h_32,q_100,w_32/logos/zero.svg',
+};
