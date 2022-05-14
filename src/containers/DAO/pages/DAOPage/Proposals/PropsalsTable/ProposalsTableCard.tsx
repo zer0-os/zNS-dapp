5293644c0c8dbf725249dd451d96c174918a42ab
joon.choi0918 <joon.choi0918@gmail.com>
2022-05-13 20:30:43 -0400

    implement proposal list and grid item

--- src/containers/DAO/pages/DAOPage/Proposals/PropsalsTable/ProposalsTableCard.tsx
+++ src/containers/DAO/pages/DAOPage/Proposals/PropsalsTable/ProposalsTableCard.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+// Hooks
+import useProposalMetadata from '../../hooks/useProposalMetadata';
+
+// Lib
+import moment from 'moment';
+import { formatTotalAmountOfTokenMetadata } from './ProposalsTable.helpers';
+import { truncateString } from 'lib/utils/string';
 
 // Components
-import { Detail } from 'components';
+import { Chiclet } from 'components';
 
 // Styles
+import classNames from 'classnames';
 import styles from './ProposalsTableCard.module.scss';
 
 // Types
-import { Proposal, TokenMetaData } from '@zero-tech/zdao-sdk';
+import { Proposal } from '@zero-tech/zdao-sdk';
+
+// Constants
+import { DEFAULT_TIMMER_EXPIRED_LABEL } from './ProposalsTable.constants';
+import { CURRENCY } from 'constants/currency';
+import { ChicletType } from 'components/Chiclet/Chiclet';
 
 interface ProposalsTableCardProps {
 	data: Proposal;
@@ -15,23 +30,64 @@ interface ProposalsTableCardProps {
 	className: string;
 }
 
-/**
- * A single grid item for the Proposals table
- */
-// TODO:: should be styled and get token metadata and format the ammount (cc @Joon)
-// TODO:: should implement timer with "end" date time (cc @Joon)
 const ProposalsTableCard: React.FC<ProposalsTableCardProps> = ({
 	data,
 	onRowClick,
 	className,
 }) => {
-	const { title, body } = data;
+	const history = useHistory();
+	const location = useLocation();
+
+	const { metadata, isLoading: isMetadataLoading } = useProposalMetadata(data);
+
+	const { id, title, body, state, end } = data;
+
+	const cardData = useMemo(() => {
+		const wild = formatTotalAmountOfTokenMetadata(metadata);
+		const isConcluded = moment(end).isBefore(moment());
+		const timeDiff = moment(end).diff(moment());
+
+		let closingType: ChicletType = 'normal';
+		if (!isConcluded && timeDiff < 24 * 3600 * 1000) {
+			// less than 1 hour
+			closingType = 'error';
+		} else if (!isConcluded && timeDiff <= 24 * 3600 * 1000) {
+			// less than 1 day
+			closingType = 'warning';
+		}
+
+		return {
+			wild: (wild || '0.00') + ' ' + CURRENCY.WILD,
+			closing: {
+				type: closingType,
+				message: isConcluded
+					? DEFAULT_TIMMER_EXPIRED_LABEL
+					: 'Closing in ' + moment.duration(timeDiff).humanize(),
+			},
+		};
+	}, [metadata, end]);
+
+	const handleCardClick = useCallback(() => {
+		history.push(`${location.pathname}/${id}`);
+		onRowClick && onRowClick();
+	}, [onRowClick, history, location, id]);
 
 	return (
-		<div className="make this card component">
-			<Detail className={styles.AmountInUSD} text={title} subtext={body} />
-
-			{/* TODO:: Should implement chicklets with prices, state and humanized time */}
+		<div
+			className={classNames(styles.Container, className)}
+			onClick={handleCardClick}
+		>
+			<div className={styles.Content}>
+				<h2 className={styles.Title}>{title}</h2>
+				<p className={styles.Description}>{truncateString(body, 150)}</p>
+			</div>
+			<div className={styles.Buttons}>
+				{!isMetadataLoading && <Chiclet>{cardData.wild}</Chiclet>}
+				<Chiclet type={cardData.closing.type}>
+					{cardData.closing.message}
+				</Chiclet>
+				<Chiclet className={styles.Status}>{state}</Chiclet>
+			</div>
 		</div>
 	);
 };
--- src/containers/DAO/pages/DAOPage/Proposals/PropsalsTable/ProposalsTableRow.tsx
+++ src/containers/DAO/pages/DAOPage/Proposals/PropsalsTable/ProposalsTableRow.tsx
@@ -1,15 +1,32 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
+// Hooks
+import useProposalMetadata from '../../hooks/useProposalMetadata';
+import useCurrency from 'lib/hooks/useCurrency';
+import useTimer from 'lib/hooks/useTimer';
+
 // Lib
-import { formatTotalAmountOfTokenMetadata } from './ProposalsTable.helpers';
+import moment from 'moment';
+import {
+	formatProposalEndTime,
+	formatTotalAmountOfTokenMetadata,
+	formatAmountInUSDOfTokenMetadata,
+} from './ProposalsTable.helpers';
+
+// Components
+import { LoadingIndicator } from 'components';
 
 // Styles
 import classNames from 'classnames';
 import styles from './ProposalsTableRow.module.scss';
 
 // Types
-import { Proposal, TokenMetaData } from '@zero-tech/zdao-sdk';
+import { Proposal } from '@zero-tech/zdao-sdk';
+
+// Constants
+import { DEFAULT_TIMMER_INTERVAL } from './ProposalsTable.constants';
+import { CURRENCY } from 'constants/currency';
 
 interface ProposalsTableRowProps {
 	data: Proposal;
@@ -20,8 +37,6 @@ interface ProposalsTableRowProps {
 /**
  * A single row item for the Proposals table
  */
-// TODO:: should be styled and get token metadata and format the ammount (cc @Joon)
-// TODO:: should implement timer with "end" date time (cc @Joon)
 const ProposalsTableRow: React.FC<ProposalsTableRowProps> = ({
 	data,
 	onRowClick,
@@ -30,7 +45,25 @@ const ProposalsTableRow: React.FC<ProposalsTableRowProps> = ({
 	const history = useHistory();
 	const location = useLocation();
 
-	const { id, title, state } = data;
+	const { metadata, isLoading: isMetadataLoading } = useProposalMetadata(data);
+
+	const { wildPriceUsd } = useCurrency(false);
+
+	const { id, title, state, end } = data;
+
+	const isConcluded = moment(end).isBefore(moment());
+
+	const { time } = useTimer(end, isConcluded ? null : DEFAULT_TIMMER_INTERVAL);
+
+	const amount = useMemo(() => {
+		const wild = formatTotalAmountOfTokenMetadata(metadata);
+		const usd = formatAmountInUSDOfTokenMetadata(wildPriceUsd, metadata);
+
+		return {
+			wild: wild ? wild + ' ' + CURRENCY.WILD : '',
+			usd: usd ?? '',
+		};
+	}, [metadata, wildPriceUsd]);
 
 	const handleRowClick = useCallback(() => {
 		history.push(`${location.pathname}/${id}`);
@@ -43,20 +76,24 @@ const ProposalsTableRow: React.FC<ProposalsTableRowProps> = ({
 			onClick={handleRowClick}
 		>
 			{/* Title */}
-			<td>{title}</td>
+			<td className={styles.Title}>{title}</td>
 
 			{/* Status */}
-			<td>{state}</td>
+			<td className={styles.Status}>{state}</td>
 
 			{/* Closes with humanized format */}
-			<td>{'2h 39mins'}</td>
+			<td className={styles.Timer}>{formatProposalEndTime(time)}</td>
 
 			{/* Total amount of tokens */}
-			<td className={styles.Right}>
-				{/* {formatTotalAmountOfTokenMetadata({
-					amount: "100,000",
-					decimals: 100000
-				} as TokenMetaData)} */}
+			<td className={styles.Amount}>
+				{isMetadataLoading ? (
+					<LoadingIndicator text="" />
+				) : (
+					<>
+						<p>{amount.wild}</p>
+						<p>{amount.usd}</p>
+					</>
+				)}
 			</td>
 		</tr>
 	);
