3b55f591d2581bba8865128b701a474204e64fd0
dominic <domw30@gmail.com>
2022-05-18 10:27:53 +0100

    fix: remove duplicated root domain when clicking zna segment

--- src/components/ZNALink/ZNALink.tsx
+++ src/components/ZNALink/ZNALink.tsx
@@ -35,9 +35,13 @@ const ZNALink: React.FC<ZNAProps> = ({ className, style }) => {
 	const segments = splitZna.map((s, index) => {
 		const name = index === 0 ? s : s;
 
+		console.log(index);
+
 		let location =
 			ROOT_DOMAIN !== ''
-				? splitZna.slice(1, index).concat(name).join('.')
+				? index === 0
+					? ''
+					: splitZna.slice(1, index).concat(name).join('.')
 				: index === 0
 				? name
 				: splitZna.slice(0, index).concat(name).join('.');
