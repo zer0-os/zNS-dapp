042a08140de15871532e031b25de494fd9dc4ef4
Brett <colbr243@gmail.com>
2022-06-21 20:10:20 +1200

    feat: zDAO Proposal MVP (#596)
    * Update dao proposals with voting SDK integration
    
    * Update amount and vote submitted attributes and used background fetching in useProposal hook to prevent whole page reloading after voting
    
    * Add warning notification for the voting power is 0
    
    * Get rid of USD amount and update amount with proposal's scores in proposals table and grid
    
    * update sdk version
    
    * fix amount text style in the proposals table row
    
    * fix markdown text preview format in proposals card
    
    * update markdown text preview color to grey in proposals card
    
    * fix grid column style issue for show more/less button position
    
    * fix vote history table border top color
    
    * create new component EtherscanLink and use it in all address presentations
    
    * Keep proposals table/grid when it is back from proposal details page
    
    * Change receiptant address to dao gnosis safe address
    
    * Update proposal status for no votes
    
    * Update design of footer for concluded proposal
    
    * Fix wrong  count timer thresold color for error
    
    * Change copy on empty vote history table
    
    * Add specific ‘Status’ copy when votes are 50% Approve, 50% Deny
    
    * Update sdk verion to fix votable for the proposals created before the token is created
    
    * Update error copy when user rejects transaction
    
    * Fix mobile responsive - proposal page
    
    * Fix format of voting power and proposal amount
    
    * Update sdk version to update proposal.votes immediately after voting
    
    * Update attributes to be not relying proposal.metadata
    
    * Hide proposals which were created in Snapshot
    
    * Update the status to 'No Votes' for closed proposals
    
    * Hide proposals tab when dao has no ens
    
    * Switch to ProposalsProvider to keep fetched proposals and show refetching loading on mount the proposals page
    
    * Fix refreshing the dApp temporarily shows the zNA bar before flicking to title bar
    
    * Hide anything to do with ‘Amount’ and ‘Funding’ and update Attributes breakpoint and count
    
    * Update status when a proposal is created in Snapshot and has more than 2 vote options in proposals table/grid and attributes
    
    * Fix feedback of iteration 5
    
    * Update sdk version to update proposal list's votes count immediately
    
    * Fix feedback of iteration 6
    
    * Fix some css
    
    * Fix mainnet DAO issue
    
    * Fix rinkeby DAO issue and hide 0://wilder DAO in MVP
    
    * Fix page reloading issue
    
    * Update vote direction color for the snapshot proposal votes
    
    * Update sdk version to update proposal detail's votes count immediately
    
    * Update the ipfs gateway to load faster
    
    * Update the note to trigger checks
    
    * Update package version number
    
    Co-authored-by: ericlow <eric.low.dev@gmail.com>
    Co-authored-by: joon.choi0918 <joon.choi0918@gmail.com>

--- src/lib/dao/providers/ZdaoSdkProvider.tsx
+++ src/lib/dao/providers/ZdaoSdkProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import {
 	chainIdToNetworkType,
