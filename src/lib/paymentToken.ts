1fee674b449c2f26e0c3b33aa789d5301fd90acc
Subrahmanyam <subrahmanyam03@gmail.com>
2022-07-04 23:28:44 +0530

    feat(zauction): add helper method to get payment token info with mock data

---
+++ src/lib/paymentToken.ts
@@ -0,0 +1,43 @@
+import { ConvertedTokenInfo } from '@zero-tech/zns-sdk';
+import * as ZNS from '@zero-tech/zns-sdk';
+
+interface PaymentTokenCache {
+	[index: string]: ConvertedTokenInfo;
+}
+
+var paymentTokenCache: PaymentTokenCache = {};
+const IS_TEST_MODE = true;
+
+const getPaymentTokenInfo = async (
+	sdk: ZNS.Instance,
+	tokenId: string,
+): Promise<ConvertedTokenInfo> => {
+	if (paymentTokenCache[tokenId]) {
+		return paymentTokenCache[tokenId];
+	} else {
+		if (IS_TEST_MODE) {
+			if (tokenId === '0x3Ae5d499cfb8FB645708CC6DA599C90e64b33A79') {
+				return {
+					id: tokenId,
+					name: 'WILD',
+					symbol: 'WILD',
+					priceInUsd: '0.185',
+					decimals: '18',
+				};
+			} else if (tokenId === '0x5bAbCA2Af93A9887C86161083b8A90160DA068f2') {
+				return {
+					id: tokenId,
+					name: 'ZERO',
+					symbol: 'ZERO',
+					priceInUsd: '0.032',
+					decimals: '18',
+				};
+			}
+		}
+		const data = await sdk.zauction.getPaymentTokenInfo(tokenId);
+		paymentTokenCache[tokenId] = data;
+		return paymentTokenCache[tokenId];
+	}
+};
+
+export default getPaymentTokenInfo;
