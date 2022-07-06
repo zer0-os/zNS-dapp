4340cf71a3de00219ce8a4305a328070133b2774
dominic <domw30@gmail.com>
2022-07-06 12:22:53 +0100

    refactor: tidy up filtering util for search domains and network switching

--- src/lib/utils/searchResults.ts
+++ src/lib/utils/searchResults.ts
@@ -1,17 +1,12 @@
 //- Library Improts
+import { ROOT_DOMAIN } from 'constants/domains';
 import { DisplayParentDomain } from 'lib/types';
 
 export const filterSearchResultsByNetwork = (
 	results: DisplayParentDomain[],
 ) => {
-	if (!results) {
-		return results;
-	} else if ((process.env.REACT_APP_NETWORK ?? '') === '') {
-		return results;
-	} else {
-		const onNetworkSearchResults = results?.filter(
-			(result) => result.name.split('.')[0] === process.env.REACT_APP_NETWORK,
-		);
-		return onNetworkSearchResults;
+	if (results && ROOT_DOMAIN) {
+		return results.filter((r) => r.name.startsWith(ROOT_DOMAIN + '.'));
 	}
+	return results;
 };
