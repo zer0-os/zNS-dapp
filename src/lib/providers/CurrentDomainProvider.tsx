e459e5a39e7fa376f33834321e363816a37fafaa
DomW <domw30@gmail.com>
2022-07-21 03:42:12 +0100

    feat: improve title switching - add documentTitle util and tests (#624)
    * feat: improve title switching - add document title util and tests
    
    * refactor: rename useDocumentTitle -> updateDocumentTitle
    
    * fix: resolve broken tests - import correct util

--- src/lib/providers/CurrentDomainProvider.tsx
+++ src/lib/providers/CurrentDomainProvider.tsx
@@ -3,19 +3,21 @@ import React, { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // Web3 Imports
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { ConvertedTokenInfo } from '@zero-tech/zns-sdk';
+
+//- Library Imports
+import { updateDocumentTitle } from 'lib/utils/documentTitle';
+import { defaultNetworkId } from 'lib/network';
+import { appFromPathname, getDomainId, zNAFromPathname } from 'lib/utils';
 import { useZnsDomain } from 'lib/hooks/useZnsDomain';
 import { usePropsState } from 'lib/hooks/usePropsState';
 import { DisplayParentDomain, Maybe, Metadata } from 'lib/types';
-import { useWeb3React } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
-import { defaultNetworkId } from 'lib/network';
-import { appFromPathname, getDomainId, zNAFromPathname } from 'lib/utils';
-import { ConvertedTokenInfo } from '@zero-tech/zns-sdk';
 
 // Constants Imports
 import { IS_DEFAULT_NETWORK, ROOT_DOMAIN } from '../../constants/domains';
 import { ROUTES } from 'constants/routes';
-
 export const CurrentDomainContext = React.createContext({
 	domain: undefined as Maybe<DisplayParentDomain>,
 	domainId: '',
@@ -28,17 +30,14 @@ export const CurrentDomainContext = React.createContext({
 	paymentToken: undefined as Maybe<string>,
 	paymentTokenInfo: {} as ConvertedTokenInfo,
 });
-
 const CurrentDomainProvider: React.FC = ({ children }) => {
 	//////////////////////////
 	// Hooks & State & Data //
 	//////////////////////////
-
 	const { chainId } = useWeb3React<Web3Provider>(); // get provider for connected wallet
 
 	// Get current domain from react-router-dom
 	const { pathname } = useLocation();
-
 	const domain = zNAFromPathname(pathname);
 
 	const zna =
@@ -46,10 +45,12 @@ const CurrentDomainProvider: React.FC = ({ children }) => {
 		(domain.length ? (IS_DEFAULT_NETWORK ? domain : '.' + domain) : '');
 
 	const domainId = getDomainId(zna);
+
 	const znsDomain = useZnsDomain(domainId, chainId || defaultNetworkId);
 	const [domainMetadata, setDomainMetadata] = usePropsState(
 		znsDomain.domainMetadata,
 	);
+
 	const appPathname = appFromPathname(pathname);
 	const app =
 		appPathname === ROUTES.STAKING
@@ -57,15 +58,7 @@ const CurrentDomainProvider: React.FC = ({ children }) => {
 			: appPathname;
 
 	// Change document title based on current network
-	if (
-		zna.length > 0 &&
-		zna !== process.env.REACT_APP_NETWORK &&
-		domainMetadata?.title
-	) {
-		document.title = process.env.REACT_APP_TITLE + ' | ' + domainMetadata.title;
-	} else {
-		document.title = process.env.REACT_APP_TITLE + ' | NFTs';
-	}
+	updateDocumentTitle(zna, app, domainMetadata?.title);
 
 	const contextValue = {
 		domain: znsDomain.domain,
@@ -82,16 +75,13 @@ const CurrentDomainProvider: React.FC = ({ children }) => {
 			id: znsDomain.paymentToken || '',
 		},
 	};
-
 	return (
 		<CurrentDomainContext.Provider value={contextValue}>
 			{children}
 		</CurrentDomainContext.Provider>
 	);
 };
-
 export default CurrentDomainProvider;
-
 export function useCurrentDomain() {
 	return useContext(CurrentDomainContext);
 }
