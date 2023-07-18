dbc40fc8653bb4ae20bba390a5598208c06aabba
Brett <12437916+colbr@users.noreply.github.com>
2023-07-18 12:46:41 +1200

    build: craco fixes

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,4 @@
 const path = require('path');
-const fs = require('fs');
-const cracoBabelLoader = require('craco-babel-loader');
-
-// manage relative paths to packages
-const appDirectory = fs.realpathSync(process.cwd());
-const resolvePackage = (relativePath) =>
-	path.resolve(appDirectory, relativePath);
 
 module.exports = {
 	webpack: {
@@ -18,22 +11,10 @@ module.exports = {
 			return webpackConfig;
 		},
 		alias: {
-			react: path.resolve('./node_modules/react'),
-			'react-dom': path.resolve('./node_modules/react-dom'),
-			'react-router': path.resolve('./node_modules/react-router'),
-			'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+			react: path.resolve('node_modules/react'),
+			'react-dom': path.resolve('node_modules/react-dom'),
+			'react-router': path.resolve('node_modules/react-router'),
+			'react-router-dom': path.resolve('node_modules/react-router-dom'),
 		},
 	},
-	plugins: [
-		{
-			plugin: cracoBabelLoader,
-			options: {
-				includes: [
-					resolvePackage('node_modules/@radix-ui/react-tooltip'),
-					resolvePackage('node_modules/@zero-tech/zui'),
-					// resolvePackage('node_modules/another-package-to-transpile'),
-				],
-			},
-		},
-	],
 };
